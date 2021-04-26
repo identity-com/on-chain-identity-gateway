481fa441773477f05ded52da1367362d6e3db3dc
Kevin Colgan <kcolgan@yahoo.com>
2021-04-26 11:10:48 +0200

    fix: removed erroneous comment

--- nyc.config.js
+++ nyc.config.js
@@ -5,7 +5,7 @@ module.exports = {
   reporter: 'lcov',
   all: true,
   lines: 100,
-  functions: 100, // coverage is actually higher than this, there is a yarn/nyc issue
+  functions: 100,
   statements: 100,
   branches: 84,
   include: ["src/**"],
