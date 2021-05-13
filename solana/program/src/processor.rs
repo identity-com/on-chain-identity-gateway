be9dd0a1259ffc505debaa168c9d7be85d16a410
dankelleher <dankelleher@yahoo.com>
2021-05-13 17:21:25 +0200

    IDCOM-300 Add Gatekeeper instruction and processor

--- solana/program/src/processor.rs
+++ solana/program/src/processor.rs
@@ -23,6 +23,7 @@ use {
         borsh::{try_from_slice_incomplete, get_instance_packed_len}
     }
 };
+use crate::state::{get_gatekeeper_address_with_seed, GATEKEEPER_ADDRESS_SEED};
 
 /// Instruction processor
 pub fn process_instruction(
@@ -33,15 +34,71 @@ pub fn process_instruction(
     let instruction = GatewayInstruction::try_from_slice(input)?;
 
     match instruction {
+        GatewayInstruction::AddGatekeeper { } => add_gatekeeper(accounts),
         GatewayInstruction::IssueVanilla { seed  } => issue_vanilla(accounts, &seed)
     }
+}
+
+fn add_gatekeeper(accounts: &[AccountInfo]) -> ProgramResult {
+    msg!("GatewayInstruction::AddGatekeeper");
+    let account_info_iter = &mut accounts.iter();
+    let funder_info = next_account_info(account_info_iter)?;
+    let gatekeeper_account_info = next_account_info(account_info_iter)?;
+    let gatekeeper_authority_info = next_account_info(account_info_iter)?;
+    let gatekeeper_network_info = next_account_info(account_info_iter)?;
+
+    let rent_info = next_account_info(account_info_iter)?;
+    let system_program_info = next_account_info(account_info_iter)?;
+    let rent = &Rent::from_account_info(rent_info)?;
+
+    let (gatekeeper_address, gatekeeper_bump_seed) = get_gatekeeper_address_with_seed(
+        gatekeeper_authority_info.key
+    );
+    if gatekeeper_address != *gatekeeper_account_info.key {
+        msg!("Error: gatekeeper account address derivation mismatch");
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    let data_len = gatekeeper_account_info.data.borrow().len();
+    if data_len > 0 {
+        msg!("Error: gatekeeper account already initialized");
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    let gatekeeper_signer_seeds: &[&[_]] =
+        &[&gatekeeper_authority_info.key.to_bytes(), GATEKEEPER_ADDRESS_SEED, &[gatekeeper_bump_seed]];
+
+    let gatekeeper_account = Gatekeeper {
+        authority: *gatekeeper_authority_info.key,
+        network: *gatekeeper_network_info.key
+    };
+    let size = get_instance_packed_len(&gatekeeper_account).unwrap() as u64;
+
+    invoke_signed(
+        &system_instruction::create_account(
+            funder_info.key,
+            gatekeeper_account_info.key,
+            1.max(rent.minimum_balance(size as usize)),
+            size,
+            &id(),
+        ),
+        &[
+            funder_info.clone(),
+            gatekeeper_authority_info.clone(),
+            system_program_info.clone(),
+        ],
+        &[&gatekeeper_signer_seeds],
+    )?;
+
+    gatekeeper_account.serialize(&mut *gatekeeper_account_info.data.borrow_mut())
+        .map_err(|e| e.into()) as ProgramResult
 }
 
 fn issue_vanilla(accounts: &[AccountInfo], seed: &Option<[u8; 8]>) -> ProgramResult {
     msg!("GatewayInstruction::IssueVanilla");
     let account_info_iter = &mut accounts.iter();
     let funder_info = next_account_info(account_info_iter)?;
-    let data_info = next_account_info(account_info_iter)?;
+    let gateway_token_info = next_account_info(account_info_iter)?;
 
     let owner_info = next_account_info(account_info_iter)?;
     let gatekeeper_account_info = next_account_info(account_info_iter)?;
@@ -58,12 +115,12 @@ fn issue_vanilla(accounts: &[AccountInfo], seed: &Option<[u8; 8]>) -> ProgramRes
         owner_info.key,
         &seed
     );
-    if gateway_token_address != *data_info.key {
+    if gateway_token_address != *gateway_token_info.key {
         msg!("Error: gateway_token address derivation mismatch");
         return Err(ProgramError::InvalidArgument);
     }
 
-    let data_len = data_info.data.borrow().len();
+    let data_len = gateway_token_info.data.borrow().len();
     if data_len > 0 {
         msg!("Error: Gateway_token account already initialized");
         return Err(ProgramError::AccountAlreadyInitialized);
@@ -86,25 +143,24 @@ fn issue_vanilla(accounts: &[AccountInfo], seed: &Option<[u8; 8]>) -> ProgramRes
         owner_info.key,
         gatekeeper_network_info.key,
         gatekeeper_account_info.key);
-    //let size = GatewayToken::try_to_vec(&gateway_token).unwrap().len() as u64;
     let size = get_instance_packed_len(&gateway_token).unwrap() as u64;
 
     invoke_signed(
         &system_instruction::create_account(
             funder_info.key,
-            data_info.key,
+            gateway_token_info.key,
             1.max(rent.minimum_balance(size as usize)),
             size,
             &id(),
         ),
         &[
             funder_info.clone(),
-            data_info.clone(),
+            gateway_token_info.clone(),
             system_program_info.clone(),
         ],
         &[&gateway_token_signer_seeds],
     )?;
 
-    gateway_token.serialize(&mut *data_info.data.borrow_mut())
+    gateway_token.serialize(&mut *gateway_token_info.data.borrow_mut())
         .map_err(|e| e.into()) as ProgramResult
 }
