2227c28b27554a66301f54fc51b29e14efd1afd9
dankelleher <dankelleher@yahoo.com>
2021-05-20 11:55:07 +0200

    IDCOM-282 fixing some compilation issues on the functional tests

--- solana/program/tests/gateway_context.rs
+++ solana/program/tests/gateway_context.rs
@@ -1,19 +1,29 @@
-use solana_program_test::ProgramTestContext;
+use solana_program_test::{ProgramTestContext, ProgramTest, processor};
 use solana_gateway_program::{
-    instruction::add_gatekeeper,
     state::{get_gatekeeper_address_with_seed, Gatekeeper},
-    instruction
+    id, instruction,
+    processor::process_instruction
 };
-use solana_sdk::transaction::Transaction;
-use solana_sdk::transport;
-use solana_sdk::signature::Keypair;
+use solana_sdk::{
+    transport,
+    transaction::Transaction,
+    signature::Keypair
+};
 use solana_program::pubkey::Pubkey;
+use solana_gateway::{
+    borsh as program_borsh,
+};
+use solana_sdk::signature::Signer;
+
+fn program_test() -> ProgramTest {
+    ProgramTest::new("solana_gateway_program", id(), processor!(process_instruction))
+}
 
 pub struct GatewayContext {
     pub context: ProgramTestContext
 }
 impl GatewayContext {
-    fn new() -> Self {
+    pub async fn new() -> Self {
         let context = program_test().start_with_context().await;
         Self {
             context
@@ -24,7 +34,15 @@ impl GatewayContext {
         &mut self,
         authority: &Pubkey,
         network: &Keypair,
+        sign_with_network: bool
     ) -> transport::Result<()> {
+        let signing_keypairs: [&Keypair;2] = 
+            if sign_with_network { 
+                [&self.context.payer, &network]
+            } else {
+                [&self.context.payer, &self.context.payer]
+            };
+        
         let transaction = Transaction::new_signed_with_payer(
             &[instruction::add_gatekeeper(
                 &self.context.payer.pubkey(),
@@ -32,18 +50,22 @@ impl GatewayContext {
                 &network.pubkey(),
             )],
             Some(&self.context.payer.pubkey()),
-            &[&self.context.payer, &network],
+            &signing_keypairs,
             self.context.last_blockhash,
         );
         self.context.banks_client.process_transaction(transaction).await
     }
     
-    pub async fn add_gatekeeper(&mut self) -> Gatekeeper {
+    pub async fn add_gatekeeper(
+        &mut self,
+        authority: &Pubkey,
+        network: &Keypair,
+    ) -> Gatekeeper {
         let (gatekeeper_address, _) = get_gatekeeper_address_with_seed(&authority);
-        add_gatekeeper_transaction(&authority, &network)
+        self.add_gatekeeper_transaction(&authority, &network, true)
             .await
             .unwrap();
-        let account_info = self.program_test_context
+        let account_info = self.context
             .banks_client
             .get_account(gatekeeper_address)
             .await
