32f23fb8d24724a21c7af5f862e0c94fe2599272
dankelleher <dankelleher@yahoo.com>
2021-05-20 11:55:07 +0200

    IDCOM-282 fixing some compilation issues on the functional tests

--- solana/program/src/state.rs
+++ solana/program/src/state.rs
@@ -18,15 +18,17 @@ pub struct Gatekeeper {
     pub network: Pubkey,
 }
 
+/// Defines an object that has a state, that can be transitioned
 pub trait Transitionable<State> {
-    fn is_valid_state_change(&self, new_state: State) -> bool;
+    /// Defines rules for transitioning an object
+    fn is_valid_state_change(&self, new_state: &State) -> bool;
 }
 impl Transitionable<GatewayTokenState> for GatewayToken {
     /// defines the set of valid state transitions for gateway tokens.
     /// Active GTs can be frozen or revoked
     /// Frozen GTs can be unfrozen (active) or revoked
     /// Revoked GTs cannot be transitioned
-    fn is_valid_state_change(&self, new_state: GatewayTokenState) -> bool {
+    fn is_valid_state_change(&self, new_state: &GatewayTokenState) -> bool {
         match new_state {
             GatewayTokenState::Active => {
                 match self.state {
