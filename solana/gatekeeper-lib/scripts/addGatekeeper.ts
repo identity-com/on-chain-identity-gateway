8647a7921bc73db30cf020f015d1877ce290b433
dankelleher <dankelleher@yahoo.com>
2021-06-30 09:37:13 +0200

    IDCOM-197 Update civicnet urls and adapt addGatekeeper script for mainnet

--- solana/gatekeeper-lib/scripts/addGatekeeper.ts
+++ solana/gatekeeper-lib/scripts/addGatekeeper.ts
@@ -1,4 +1,4 @@
-import { Connection, Keypair } from "@solana/web3.js";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { SOLANA_COMMITMENT } from "../src/util/constants";
 import { GatekeeperNetworkService } from "../src/service/GatekeeperNetworkService";
 import { homedir } from "os";
@@ -8,15 +8,21 @@ const mySecretKey = require(path.join(
   homedir(),
   ".config",
   "solana",
-  "id.json"
+  // gatekeeper network key
+  "gatsGgZznicBQq9ybbygjizWvq5i3pTzrjTE6Cafz36.json" //"id.json"
 ));
 const myKeypair = Keypair.fromSecretKey(Buffer.from(mySecretKey));
 
-const connection = new Connection("http://localhost:8899", SOLANA_COMMITMENT);
+const connection = new Connection(
+  "https://api.mainnet-beta.solana.com",
+  SOLANA_COMMITMENT
+);
 
 const service = new GatekeeperNetworkService(connection, myKeypair, myKeypair);
 
-const gatekeeperAuthority = Keypair.generate().publicKey;
+const gatekeeperAuthority = new PublicKey(
+  "civQnFJNKpRpyvUejct4mfExBi7ZzRXu6U3hXWMxASn"
+); //Keypair.generate().publicKey;
 
 (async function () {
   const gatekeeperAccount = await service.addGatekeeper(gatekeeperAuthority);
