b307442a98f7d53eea52fb9ba2c534297a394de5
bahadylbekov <33404905+bahadylbekov@users.noreply.github.com>
2021-08-02 15:40:06 +0300

    fix: added missed variables to the deployment script

--- deploy/1_deploy_gateway_token_controller.js
+++ deploy/1_deploy_gateway_token_controller.js
@@ -11,7 +11,7 @@ module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   })
 
   const tokenController = await ethers.getContract("GatewayTokenController")
-  let gatewayToken = await (await tokenController.createGatekeeperNetwork("Test-KYC", "tKYC", {from: deployer})).wait()
+  let gatewayToken = await (await tokenController.createGatekeeperNetwork("Test-KYC", "tKYC", false, "0x0000000000000000000000000000000000000000", {from: deployer})).wait()
 
   console.log("deployed tKYC GatewayToken at " + gatewayToken.events[gatewayToken.events.length - 1].args.tokenAddress + " with " + gatewayToken.gasUsed.toNumber() + " gas");
 }
