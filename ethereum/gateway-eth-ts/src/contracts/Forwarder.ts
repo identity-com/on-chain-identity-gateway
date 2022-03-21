064e16991165712c9bf7792eb4681a96d30fbe62
TYRONE AVNIT <tyronemichael@gmail.com>
2022-03-21 17:03:25 +0200

    CG-324 - Update gateway-ethereum-ts to use sendable transaction type

---
+++ ethereum/gateway-eth-ts/src/contracts/Forwarder.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Contract, Signer} from 'ethers';
+import { BaseProvider } from '@ethersproject/providers';
+import abis from '../lib/abis';
+import { EIP712Message } from '../utils/signer';
+
+export class Forwarder {
+    contract: Contract;
+
+    constructor(signerOrProvider: Signer | BaseProvider, addressOrName: string) {
+        this.contract = new Contract(
+            addressOrName,
+            abis.Forwarder,
+            signerOrProvider,
+          );      
+    }
+
+    execute = async (request: EIP712Message, signature: string) => {
+        return this.contract.execute(request, signature);
+    }
+}
--- ethereum/gateway-eth-ts/src/lib/abis/index.ts
+++ ethereum/gateway-eth-ts/src/lib/abis/index.ts
@@ -1,9 +1,11 @@
 import GatewayToken from './GatewayToken';
 import GatewayTokenController from './GatewayTokenController';
 import FlagsStorage from './FlagsStorage';
+import Forwarder from './Forwarder';
 
 export default {
     GatewayTokenController,
     GatewayToken,
     FlagsStorage,
+    Forwarder,
 }
