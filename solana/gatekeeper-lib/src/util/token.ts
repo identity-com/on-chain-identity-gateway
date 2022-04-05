822bdf7248829555789ca3c1bae0b65a9d8fbb40
Martin Riedel <web@riedel-it.de>
2022-04-05 12:35:24 -0400

    fix: updated gatewayTokenInfo

--- solana/gatekeeper-lib/src/util/token.ts
+++ solana/gatekeeper-lib/src/util/token.ts
@@ -1,42 +1,14 @@
-// eslint-disable-next-line unicorn/prefer-node-protocol
-import { Buffer } from "buffer";
 import { Connection, PublicKey } from "@solana/web3.js";
 import {
-  AccountInfo,
-  AccountLayout,
-  TOKEN_PROGRAM_ID,
+  Account,
+  getAccount,
 } from "@solana/spl-token";
 import { GatewayToken } from "@identity.com/solana-gateway-ts";
 
 export const gatewayTokenInfo = async (
   connection: Connection,
   tokenAccount: PublicKey
-): Promise<AccountInfo> => {
-  const info = await connection.getAccountInfo(tokenAccount);
-
-  if (info === null) {
-    throw new Error("Account not found");
-  }
-
-  if (!info.owner.equals(TOKEN_PROGRAM_ID)) {
-    throw new Error("Not an SPL Token");
-  }
-
-  if (info.data.length !== AccountLayout.span) {
-    throw new Error("Invalid account size");
-  }
-
-  // ? Is accountInfo NOT of type AccountInfo? I'm confused as to why state does not exist on
-  const data: Buffer = Buffer.from(info.data);
-  const accountInfo = AccountLayout.decode(data) as AccountInfo;
-  accountInfo.address = tokenAccount;
-  accountInfo.mint = new PublicKey(accountInfo.mint);
-  accountInfo.owner = new PublicKey(accountInfo.owner);
-  accountInfo.isInitialized = accountInfo.state !== 0;
-  accountInfo.isFrozen = accountInfo.state === 2;
-
-  return accountInfo;
-};
+): Promise<Account> => getAccount(connection, tokenAccount);
 
 export const prettyPrint = (token: GatewayToken): string =>
   JSON.stringify(
