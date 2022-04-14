b7f380d126c9e0823e39a0f9a2b183a88fc8d131
bahadylbekov <baha296@mail.ru>
2022-04-14 18:08:55 +0600

    fix: update library usage in examples

--- ethereum/gateway-eth-ts/examples/verify-token.js
+++ ethereum/gateway-eth-ts/examples/verify-token.js
@@ -6,12 +6,9 @@ require("dotenv/config");
     const provider = getDefaultProvider('ropsten', {infura: process.env.INFURA_KEY});
     let wallet = new Wallet(`0x${process.env.PRIVATE_KEY}`);
     wallet = wallet.connect(provider);
+    const network = await provider.getNetwork();
 
-    const gtLib = new GatewayETH(
-        provider,
-        wallet
-    );
-    await gtLib.init()
+    const gtLib = new GatewayETH(wallet, network);
 
     const testUser = '0x57AB42d4fa756b6956b0cAf986a5f53bA90D9e28';
     let tx = await gtLib.verify(testUser);
