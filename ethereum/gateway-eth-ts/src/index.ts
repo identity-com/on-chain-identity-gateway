dd753658e4e8701d01d337d7a480244d6b68ad71
Flippie Scholtz <flippie@civic.com>
2022-04-20 23:13:49 +0200

    CIV-4725 Allow Provider to be passed in to GatewayETH constructur.

--- ethereum/gateway-eth-ts/src/index.ts
+++ ethereum/gateway-eth-ts/src/index.ts
@@ -1,6 +1,6 @@
 import { GatewayTs } from "./GatewayTs";
 import { Signer, utils } from "ethers";
-import { Network } from "@ethersproject/providers";
+import { Network, Provider } from "@ethersproject/providers";
 export { GasPriceKey } from "./utils/gas";
 export { TokenData, SendableTransaction, SentTransaction } from "./utils/types";
 export { DEFAULT_GATEWAY_TOKEN } from "./utils/constants";
@@ -15,19 +15,19 @@ export class GatewayETH extends GatewayTs {
    * const {GatewayETH} = require('GatewayTS');
    * const gatewayTs = new GatewayETH();
    * @constructor
-   * @param signer {Ethers signer, gets default provider if there is no signer provided}
+   * @param providerOrSigner {Ethers signer or provider, gets default provider if not provided}
    * @param network {Ethers network class}
    * @param defaultGatewayToken {Address of default gateway token contract}
    * @param options {Gas, gasPrice}
    * @notice utils {Ethers utils}
    */
   constructor(
-    signer: Signer,
+    providerOrSigner: Provider | Signer,
     network: Network,
     defaultGatewayToken?: string,
     options?: { defaultGas?: number; defaultGasPrice?: any }
   ) {
-    super(signer, network, defaultGatewayToken, options);
+    super(providerOrSigner, network, defaultGatewayToken, options);
     this.utils = utils;
   }
 }
