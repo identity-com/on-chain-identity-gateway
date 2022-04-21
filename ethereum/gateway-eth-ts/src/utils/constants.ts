a3277ee01b19d93dacbd3000b96c3fe33e572ad6
Flippie Scholtz <flippie@civic.com>
2022-04-21 15:29:47 +0200

    CIV-4725 Fix test by changing back DEFAULT_GATEWAY_TOKEN to what it was.

--- ethereum/gateway-eth-ts/src/utils/constants.ts
+++ ethereum/gateway-eth-ts/src/utils/constants.ts
@@ -11,7 +11,7 @@ export const DEFAULT_CHAIN_ID = 3;
 export const DEFAULT_NETWORK = "ropsten";
 
 export const DEFAULT_GATEWAY_TOKEN =
-  "0xa3894BbA27f4Be571fFA319D02c122E021024cF2"; // Zambezi network on ropsten
+  "0x67306284Fb127E9baF713Ebf793d741cE763F81A";
 export const DEFAULT_MNEMONIC =
   "test test test test test test test test test test test junk";
 export const DEFAULT_GATEWAY_TOKEN_CONTROLLER =
