48edbd817a66493546c637d477d6baa51965696a
William Brooks <william@identity.org>
2022-06-14 19:16:56 +0200

    IDCOM-1986 added initial update instructions

--- solana/program_v2/src/lib.rs
+++ solana/program_v2/src/lib.rs
@@ -1,23 +1,24 @@
+#![feature(in_band_lifetimes)]
 #![warn(
-    unused_import_braces,
-    unused_imports,
-    // missing_docs,
-    missing_debug_implementations,
-    clippy::pedantic
+unused_import_braces,
+unused_imports,
+// missing_docs,
+missing_debug_implementations,
+clippy::pedantic
 )]
 #![allow(
-    clippy::cast_possible_truncation,
-    clippy::module_name_repetitions,
-    clippy::missing_errors_doc,
-    clippy::too_many_lines,
-    clippy::mut_mut,
-    clippy::wildcard_imports
+clippy::cast_possible_truncation,
+clippy::module_name_repetitions,
+clippy::missing_errors_doc,
+clippy::too_many_lines,
+clippy::mut_mut,
+clippy::wildcard_imports
 )]
 
 use anchor_lang::prelude::*;
 use crate::account::{GatekeeperNetwork, NetworkAuthKey};
 
-declare_id!("9aYjKhEJd3ZoGe1TWASXw645gexZfLeQcm6W8vgF7BxH");
+declare_id!("ACFuMqpZ8bRXhhdJ2frtk62BS26D2fDgSCfw6v1er4C5");
 
 pub mod account;
 pub mod instructions;
@@ -25,72 +26,56 @@ pub mod types;
 pub mod util;
 
 use crate::account::*;
-use crate::instructions::Network;
 use crate::types::NetworkFees;
 use crate::types::NetworkKeyFlags;
+use crate::instructions::*;
 
 #[program]
 pub mod gateway_v2 {
     use super::*;
 
-    pub fn create_network(ctx: Context<CreateNetwork>, data: CreateNetworkData) -> Result<()> {
-        Network::create(data, &mut ctx.accounts.network)
+    pub fn create_network(ctx: Context<CreateNetworkAccounts>, data: CreateNetworkData) -> Result<()> {
+        CreateNetwork::process(data, &mut ctx.accounts.network)
     }
 
-    pub fn update_network(ctx: Context<UpdateNetwork>, data: UpdateNetworkData) -> Result<()> {
-        Network::update(data, &mut ctx.accounts.network)
+    pub fn update_network(ctx: Context<UpdateNetworkAccounts>, data: UpdateNetworkData) -> Result<()> {
+        UpdateNetwork::process(data, &mut ctx.accounts.network, &mut ctx.accounts.payer)
     }
 }
 
 #[derive(Accounts, Debug)]
-#[instruction(data: CreateNetworkData)]
-pub struct UpdateNetwork<'info> {
+pub struct UpdateNetworkAccounts<'info> {
     #[account(
+    mut,
+    realloc = (5000 as usize),
+    realloc::payer = payer,
+    realloc::zero = false
     )]
     pub network: Account<'info, GatekeeperNetwork>,
-    // pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts, Debug)]
-#[instruction(data: CreateNetworkData)]
-pub struct CreateNetwork<'info> {
-    #[account(
-        init,
-        payer = payer,
-        space = GatekeeperNetwork::on_chain_size_with_arg(
-            GatekeeperNetworkSize{
-                fees_count: data.fees.len() as u16,
-                auth_keys: data.auth_keys.len() as u16,
-            }
-        ),
-    )]
-    pub network: Box<Account<'info, GatekeeperNetwork>>,
     #[account(mut)]
     pub payer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-// /// Accounts for [`CreateNetwork`].
-// #[account]
-// pub struct CreateNetworkAccounts<AI> {
-//     /// The network to create.
-//     #[validate(data = GatewayNetworkCreate{
-//     system_program: &self.system_program,
-//     rent: Some(rent),
-//     funder: self.funder.as_ref(),
-//     funder_seeds: None,
-//     cpi: CPIChecked,
-//     })]
-//     pub network: GatekeeperNetworkAccount<AI>,
-//     /// The system program
-//     pub system_program: SystemProgram<AI>,
-//     /// The signer for the network.
-//     #[validate(data = (NetworkSignerSeeder{ network: *self.network.info().key() }, signer_bump))]
-//     pub network_signer: Seeds<AI, NetworkSignerSeeder>,
-//     /// The funder for the network if needed.
-//     #[validate(signer(IfSome), writable(IfSome))]
-//     pub funder: Option<AI>,
-// }
+#[derive(Accounts, Debug)]
+#[instruction(data: CreateNetworkData)]
+pub struct CreateNetworkAccounts<'info> {
+    #[account(
+    init,
+    payer = payer,
+    space = GatekeeperNetwork::on_chain_size_with_arg(
+    GatekeeperNetworkSize{
+    fees_count: data.fees.len() as u16,
+    auth_keys: data.auth_keys.len() as u16,
+    }
+    ),
+    )]
+    pub network: Account<'info, GatekeeperNetwork>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 // /// Data for [`CreateNetwork`].
 #[derive(Debug, AnchorSerialize, AnchorDeserialize)]
 pub struct CreateNetworkData {
@@ -109,15 +94,27 @@ pub struct CreateNetworkData {
 }
 
 #[derive(Debug, AnchorSerialize, AnchorDeserialize)]
+pub struct UpdateFees {
+    pub add: Vec<NetworkFees>,
+    pub remove: Vec<NetworkFees>,
+}
+
+#[derive(Debug, AnchorSerialize, AnchorDeserialize)]
+pub struct UpdateKeys {
+    pub add: Vec<NetworkAuthKey>,
+    pub remove: Vec<Pubkey>,
+}
+
+#[derive(Debug, AnchorSerialize, AnchorDeserialize)]
 pub struct UpdateNetworkData {
     /// The [`GatekeeperNetwork::auth_threshold`].
     pub auth_threshold: u8,
     /// The [`GatekeeperNetwork::pass_expire_time`].
     pub pass_expire_time: i64,
-    /// The [`GatekeeperNetwork::network_data_len`].
+    /// The [`GatekeeperNetwork::networkx_data_len`].
     pub network_data_len: u16,
     /// The [`GatekeeperNetwork::signer_bump`].
-    pub fees: Vec<NetworkFees>,
+    pub fees: UpdateFees,
     /// The [`GatekeeperNetwork::auth_keys`].
-    pub auth_keys: Vec<NetworkAuthKey>,
+    pub auth_keys: UpdateKeys,
 }
---
+++ solana/program_v2/tests/network.ts
@@ -0,0 +1,278 @@
+import * as anchor from "@project-serum/anchor";
+import {Program} from "@project-serum/anchor";
+import {GatewayV2} from "../target/types/gateway_v2";
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+describe("network operations", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.GatewayV2 as Program<GatewayV2>;
+
+  const createAccount = async (baseAccount) => {
+    await program.methods.createNetwork({
+      authThreshold: new anchor.BN(1),
+      passExpireTime: new anchor.BN(360),
+      networkDataLen: new anchor.BN(16),
+      signerBump: new anchor.BN(3),
+      fees: [{
+        token: baseAccount.publicKey,
+        issue: new anchor.BN(100),
+        refresh: new anchor.BN(100),
+        expire: new anchor.BN(100),
+        verify: new anchor.BN(100)
+      }],
+      authKeys: [
+        {
+          flags: new anchor.BN(1),
+          key: provider.wallet.publicKey
+        }
+      ],
+    }).accounts({
+      network: baseAccount.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([baseAccount]).rpc();
+
+    return program.account.gatekeeperNetwork.fetch(baseAccount.publicKey);
+  }
+
+  it("create an account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    const account = await createAccount(baseAccount);
+
+    expect(account).to.deep.equal({
+        "version": 0,
+        "authThreshold": 1,
+        "passExpireTime": new anchor.BN(360),
+        "networkDataLen": 16,
+        "signerBump": 3,
+        "feesCount": 1,
+        "authKeysCount": 1,
+        "fees": [
+          {
+            "token": baseAccount.publicKey,
+            "issue": 100,
+            "refresh": 100,
+            "expire": 100,
+            "verify": 100
+          }
+        ],
+        "authKeys": [
+          {
+            "flags": 1,
+            "key": provider.publicKey
+          }
+        ]
+      }
+    );
+  });
+
+  it("adds an auth key to an account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await createAccount(baseAccount);
+
+    await program.methods.updateNetwork({
+        authKeys: {
+          add: [{
+            flags: new anchor.BN(1),
+            key: baseAccount.publicKey
+          }],
+          remove: []
+        },
+        fees: {add: [], remove: []}
+      }
+    ).accounts({
+      network: baseAccount.publicKey,
+    }).signers([]).rpc();
+
+    const account = await program.account.gatekeeperNetwork.fetch(baseAccount.publicKey);
+
+    expect(account).to.deep.equal({
+        "version": 0,
+        "authThreshold": 1,
+        "passExpireTime": new anchor.BN(360),
+        "networkDataLen": 16,
+        "signerBump": 3,
+        "feesCount": 1,
+        "authKeysCount": 2,
+        "fees": [
+          {
+            "token": baseAccount.publicKey,
+            "issue": 100,
+            "refresh": 100,
+            "expire": 100,
+            "verify": 100
+          }
+        ],
+        "authKeys": [
+          {
+            "flags": 1,
+            "key": provider.wallet.publicKey
+          },
+          {
+            "flags": 1,
+            "key": baseAccount.publicKey
+          }
+        ]
+      }
+    );
+  });
+
+  it("removes an auth key to an account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await createAccount(baseAccount);
+
+    await program.methods.updateNetwork({
+        authKeys: {
+          add: [{
+            flags: new anchor.BN(1),
+            key: baseAccount.publicKey
+          }],
+          remove: []
+        },
+        fees: {
+          add: [],
+          remove: []
+        },
+      }
+    ).accounts({
+      network: baseAccount.publicKey,
+    }).signers([]).rpc();
+
+    await program.methods.updateNetwork({
+        authKeys: {
+          add: [],
+          remove: [baseAccount.publicKey]
+        },
+        fees: {
+          add: [],
+          remove: []
+        },
+      }
+    ).accounts({
+      network: baseAccount.publicKey,
+    }).signers([]).rpc();
+
+    const account = await program.account.gatekeeperNetwork.fetch(baseAccount.publicKey);
+
+    expect(account).to.deep.equal({
+        "version": 0,
+        "authThreshold": 1,
+        "passExpireTime": new anchor.BN(360),
+        "networkDataLen": 16,
+        "signerBump": 3,
+        "feesCount": 1,
+        "authKeysCount": 1,
+        "fees": [
+          {
+            "token": baseAccount.publicKey,
+            "issue": 100,
+            "refresh": 100,
+            "expire": 100,
+            "verify": 100
+          }
+        ],
+        "authKeys": [
+          {
+            "flags": 1,
+            "key": provider.publicKey
+          },
+        ]
+      }
+    );
+  });
+
+  it("cannot remove own account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await createAccount(baseAccount);
+
+    return expect(program.methods.updateNetwork({
+        authKeys: {
+          add: [],
+          remove: [provider.wallet.publicKey]
+        },
+        fees: {add: [], remove: []},
+      }
+    ).accounts({
+      network: baseAccount.publicKey,
+    }).signers([]).rpc()).to.eventually.be.rejected;
+  });
+
+  it("updates an auth key to an account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await createAccount(baseAccount);
+
+    await program.methods.updateNetwork({
+        authKeys: {
+          add: [{
+            flags: new anchor.BN(3),
+            key: provider.publicKey
+          }],
+          remove: []
+        },
+        fees: {add: [], remove: []}
+      }
+    ).accounts({
+      network: baseAccount.publicKey,
+    }).signers([]).rpc();
+
+    const account = await program.account.gatekeeperNetwork.fetch(baseAccount.publicKey);
+
+    expect(account).to.deep.equal({
+        "version": 0,
+        "authThreshold": 1,
+        "passExpireTime": new anchor.BN(360),
+        "networkDataLen": 16,
+        "signerBump": 3,
+        "feesCount": 1,
+        "authKeysCount": 1,
+        "fees": [
+          {
+            "token": baseAccount.publicKey,
+            "issue": 100,
+            "refresh": 100,
+            "expire": 100,
+            "verify": 100
+          }
+        ],
+        "authKeys": [
+          {
+            "flags": 3,
+            "key": provider.publicKey
+          }
+        ]
+      }
+    );
+  });
+
+  it("cannot update to remove auth flag from account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await createAccount(baseAccount);
+
+    return expect(program.methods.updateNetwork({
+        authKeys: {
+          add: [{
+            flags: new anchor.BN(2),
+            key: provider.wallet.publicKey
+          }],
+          remove: []
+        },
+        fees: {add: [], remove: []}
+      }
+    ).accounts({
+      network: baseAccount.publicKey,
+    }).signers([]).rpc()).to.eventually.be.rejected;
+  });
+
+});
