b55b4838bad6327a69945f9559d7549c46674137
dankelleher <dankelleher@yahoo.com>
2022-06-29 08:29:22 +0200

    Eth: Some Readme and client lib export fixes

--- ethereum/gateway-eth-ts/src/index.ts
+++ ethereum/gateway-eth-ts/src/index.ts
@@ -1,1 +1,4 @@
 export { GatewayTs } from "./service/GatewayTs";
+export { GatewayTsForwarder } from "./service/GatewayTsForwarder";
+export { TokenData, TokenState } from "./utils/types";
+export { generateId } from "./utils/tokenId";
