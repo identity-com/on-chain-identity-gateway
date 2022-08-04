62b23d7cbc73b4979030fcb03d78b708c0e74c3f
Frank <frank.wang@identity.com>
2022-08-03 17:20:50 -0700

    implement create network

--- solana/program_v2/client/src/lib/constants.ts
+++ solana/program_v2/client/src/lib/constants.ts
@@ -1,2 +1,10 @@
+import { PublicKey } from '@solana/web3.js';
+
 export const PROGRAM_ID = 'FSgDgZoNxiUarRWJYrMDWcsZycNyEXaME5i3ZXPnhrWe';
+
+export const GATE_WAY_PROGRAM = new PublicKey(
+    'FSgDgZoNxiUarRWJYrMDWcsZycNyEXaME5i3ZXPnhrWe'
+  );
+
+export const DEFAULT_SEED_STRING = "gateway_v2"
 
