1b7dba221410d7d5f0957eacd844e07244973286
DiosAkhilleus <abertin1809@gmail.com>
2022-08-08 11:55:24 -0500

    working on cli additions

--- solana/program_v2/cli/src/commands/network/create/index.ts
+++ solana/program_v2/cli/src/commands/network/create/index.ts
@@ -1,19 +1,74 @@
-import { Command } from "@oclif/core";
-
+import { Command, Flags } from "@oclif/core";
+import { Keypair } from "@solana/web3.js";
+import * as fs from "fs-extra";
+import { createNetwork } from "../../../utils/create-network";
+import {
+  i64,
+  NetworkAuthKey,
+  NetworkData,
+  NetworkKeyFlags,
+  NetworkKeyFlagsValues,
+  u16,
+  u8,
+} from "../../../utils/state";
 export default class Create extends Command {
   static description = "Creates a gatekeeper network";
 
   static examples = [
-    `$ gateway network create
-network created!
+    `$ gateway network create --d ./network.json --f ./funder-key.json
+Latest Blockhash: [blockhash]
 `,
   ];
 
-  static flags = {};
+  static flags = {
+    help: Flags.help({ char: "h" }),
+    data: Flags.string({
+      char: "d",
+      description: "Path to network data",
+      required: false,
+    }),
+    key: Flags.string({
+      char: "k",
+      description: "Path to a solana keypair",
+      required: false,
+    }),
+  };
 
   static args = [];
 
   async run(): Promise<void> {
-    this.log("network created!");
+    const { flags } = await this.parse(Create);
+    const programId = Keypair.generate().publicKey;
+    const network = Keypair.generate();
+    const localSecretKey = fs.readJSONSync(flags.key) as Uint8Array;
+    this.log(`${localSecretKey}`);
+    const funder = Keypair.fromSecretKey(localSecretKey);
+
+    const networkData = new NetworkData(
+      new u8(1),
+      new i64(BigInt(60) * BigInt(60)),
+      new u16(0),
+      new u8(0),
+      [],
+      [
+        new NetworkAuthKey(
+          NetworkKeyFlags.fromFlagsArray([NetworkKeyFlagsValues.AUTH]),
+          funder.publicKey
+        ),
+      ]
+    );
+
+    this.log(`Program ID: ${programId.toString()}`);
+    this.log(`Network ID: ${network.publicKey.toString()}`);
+    this.log(`Funder ID: ${funder.publicKey.toString()}`);
+    this.log(`Network Data: ${JSON.stringify(networkData)}`);
+    const createdNetwork = await createNetwork(
+      programId,
+      network,
+      funder,
+      networkData
+    );
+    this.log("network created");
+    this.log(`Block Height: ${createdNetwork.lastValidBlockHeight}`);
   }
 }
