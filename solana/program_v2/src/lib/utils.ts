196f23cec2096b2fc59d01434b9c2b52089dd6a2
DiosAkhilleus <abertin1809@gmail.com>
2022-08-17 13:34:02 -0500

    added method to utils and updated testing format

--- solana/program_v2/src/lib/utils.ts
+++ solana/program_v2/src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { Connection, Keypair } from "@solana/web3.js";
-import { AnchorProvider, Wallet, Program } from "@project-serum/anchor";
+import { AnchorProvider, Wallet, Program, web3 } from "@project-serum/anchor";
+import * as anchor from "@project-serum/anchor";
 import { IDL } from "../gateway_v2";
 
 import { PROGRAM_ID } from "./constants";
@@ -16,4 +17,19 @@ export const getAnchorProgram = (payer: Keypair) => {
   });
 
   return new Program(IDL, PROGRAM_ID, provider);
+};
+
+export const airdrop = async (
+  connection: web3.Connection,
+  account: web3.PublicKey,
+  amount = anchor.web3.LAMPORTS_PER_SOL
+) => {
+  const sigAirdrop = await connection.requestAirdrop(account, amount);
+  const latestBlockHash = await connection.getLatestBlockhash();
+
+  await connection.confirmTransaction({
+    signature: sigAirdrop,
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+  });
 };
--- solana/program_v2/tests/network-suite/create-network.test.ts
+++ solana/program_v2/tests/network-suite/create-network.test.ts
@@ -3,17 +3,32 @@ import { GatewayService } from "../../src/GatewayService";
 import { GatewayV2, IDL } from "../../src/gateway_v2";
 import * as anchor from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
+import { airdrop } from "../../src/lib/utils";
 
 describe("Gateway v2 Client", () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+  let service: GatewayService;
+
+  const program = anchor.workspace.GatewayV2 as Program<GatewayV2>;
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  const authority = programProvider.wallet;
+
+  const nonAuthoritySigner = anchor.web3.Keypair.generate();
+
+  before(async () => {
+    service = await GatewayService.buildFromAnchor(
+      program,
+      authority.publicKey,
+      "localnet",
+      programProvider
+    );
+
+    // Fund nonAuthoritySigner
+    await airdrop(programProvider.connection, nonAuthoritySigner.publicKey);
+  });
   describe("Create Network", () => {
     it.only("should be equivalent", async function () {
-      anchor.setProvider(anchor.AnchorProvider.env());
-      const program = anchor.workspace.GatewayV2 as Program<GatewayV2>;
-      const programProvider = program.provider as anchor.AnchorProvider;
-
-      const authority = programProvider.wallet;
-      let service: GatewayService;
-
       // (see sol-did didDataAccount)
       const [network, bump] = await PublicKey.findProgramAddress(
         [
