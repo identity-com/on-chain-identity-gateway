5d48c8307b6e41277f81793b365bab531ebe6c62
DiosAkhilleus <abertin1809@gmail.com>
2022-08-18 12:34:55 -0500

    formatting changes and test updates

--- solana/program_v2/src/lib/constants.ts
+++ solana/program_v2/src/lib/constants.ts
@@ -1,10 +1,9 @@
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey } from "@solana/web3.js";
 
-export const PROGRAM_ID = 'FSgDgZoNxiUarRWJYrMDWcsZycNyEXaME5i3ZXPnhrWe';
+export const PROGRAM_ID = "FSgDgZoNxiUarRWJYrMDWcsZycNyEXaME5i3ZXPnhrWe";
 
 export const GATE_WAY_PROGRAM = new PublicKey(
-    'FSgDgZoNxiUarRWJYrMDWcsZycNyEXaME5i3ZXPnhrWe'
-  );
+  "FSgDgZoNxiUarRWJYrMDWcsZycNyEXaME5i3ZXPnhrWe"
+);
 
-export const DEFAULT_SEED_STRING = "gateway_v2"
-
+export const DEFAULT_SEED_STRING = "gateway_v2";
