7cd3cf78e7d12a0932b6c00ff5ee1701d3d5f2ff
DiosAkhilleus <abertin1809@gmail.com>
2022-08-24 15:59:58 -0500

    Working on cli create network

---
+++ solana/program_v2/cli/src/utils/lib/types.ts
@@ -0,0 +1,7 @@
+import { PublicKey, Transaction } from "@solana/web3.js";
+
+export interface Wallet {
+  signTransaction(tx: Transaction): Promise<Transaction>;
+  signAllTransactions(txs: Transaction[]): Promise<Transaction[]>;
+  publicKey: PublicKey;
+}
