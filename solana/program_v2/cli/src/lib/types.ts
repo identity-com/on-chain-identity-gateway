7805ff3459e01d591cc6a922204af261fd21d488
DiosAkhilleus <abertin1809@gmail.com>
2022-09-08 10:57:26 -0500

    making progress on cli network-update... should work if enough auth access

---
+++ solana/program_v2/cli/src/lib/types.ts
@@ -0,0 +1,55 @@
+import { PublicKey, Transaction } from "@solana/web3.js";
+import * as anchor from "@project-serum/anchor";
+
+export interface Wallet {
+  signTransaction(tx: Transaction): Promise<Transaction>;
+  signAllTransactions(txs: Transaction[]): Promise<Transaction[]>;
+  publicKey: PublicKey;
+}
+
+export type FeeStructure = {
+  token: PublicKey;
+  issue: number;
+  refresh: number;
+  expire: number;
+  verify: number;
+};
+
+export type UpdateFeeStructure = {
+  add: FeeStructure[];
+  remove: PublicKey[];
+};
+
+export type UpdateAuthKeytructure = {
+  add: AuthKeyStructure[];
+  remove: PublicKey[];
+};
+
+export type AuthKeyStructure = {
+  flags: number;
+  key: PublicKey;
+};
+
+export type CreateNetworkData = {
+  authThreshold: number;
+  passExpireTime: number;
+  signerBump: number;
+  fees: FeeStructure[];
+  authKeys: AuthKeyStructure[];
+};
+
+export type UpdateNetworkData = {
+  authThreshold: number;
+  passExpireTime: number;
+  fees: UpdateFeeStructure;
+  authKeys: UpdateAuthKeytructure;
+};
+export type NetworkAccount = {
+  version: number;
+  initialAuthority: PublicKey;
+  authThreshold: number;
+  passExpireTime: number;
+  fees: FeeStructure[];
+  authKeys: AuthKeyStructure[];
+};
+// TODO!: Frankly I don't understand why these are not able to be in here. It seems like the updateNetwork() function takes them as 'never' type, which clearly is not accurate
