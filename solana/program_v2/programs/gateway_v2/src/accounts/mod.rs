274da95ae391404df312fc88a604dbe2621ecff4
William Brooks <william@identity.org>
2022-09-12 14:04:46 +0200

    deleted unused files

--- solana/program_v2/programs/gateway_v2/src/accounts/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Data accounts for this program.
-
-mod gatekeeper;
-mod network;
-mod pass;
-
-pub use gatekeeper::*;
-pub use network::*;
-pub use pass::*;
--- solana/program_v2/programs/gateway_v2/src/arguments/gatekeeper_account.rs
+++
@@ -1,6 +0,0 @@
-use crate::accounts::Gatekeeper;
-use crate::GatewayAccountList;
-use cruiser::prelude::*;
-
-/// A [`Gatekeeper`] account
-pub type GatekeeperAccount<AI> = InPlaceAccount<AI, GatewayAccountList, Gatekeeper>;
