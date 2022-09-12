7da6a4eb6e4dbe7aea33b20e3de36f581bafc727
William Brooks <william@identity.org>
2022-09-12 20:23:50 +0200

    Removed commented code

--- solana/program_v2/programs/gateway_v2/src/errors.rs
+++ solana/program_v2/programs/gateway_v2/src/errors.rs
@@ -15,19 +15,3 @@ pub enum NetworkErrors {
     #[msg("Invalid key provided")]
     InvalidKey,
 }
-
-// #[error_code]
-// pub enum UpdateNetworkErrors {
-//     #[msg("No auth keys provided")]
-//     NoAuthKeys,
-//     #[msg("Not enough auth keys provided")]
-//     InsufficientAuthKeys,
-//     #[msg("Invalid key provided")]
-//     InvalidKey,
-//     #[msg("Insufficient access to update auth keys")]
-//     InsufficientAccessAuthKeys,
-//     #[msg("Insufficient access to set expiry time")]
-//     InsufficientAccessExpiry,
-//     #[msg("Auth key not found")]
-//     AuthKeyNotFound,
-// }
