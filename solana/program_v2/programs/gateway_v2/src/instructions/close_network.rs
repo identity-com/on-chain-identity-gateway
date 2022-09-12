735a482c7c04fe77ea2b34a9137e97a29b8f4e62
William Brooks <william@identity.org>
2022-09-12 21:23:35 +0200

    Fixed constraint on close network

--- solana/program_v2/programs/gateway_v2/src/instructions/close_network.rs
+++ solana/program_v2/programs/gateway_v2/src/instructions/close_network.rs
@@ -1,7 +1,7 @@
 use crate::constants::NETWORK_SEED;
 use crate::state::GatekeeperNetwork;
 use anchor_lang::prelude::*;
-
+use crate::types::*;
 /// Placeholder for additional close_network functionality
 pub fn close_network() -> Result<()> {
     Ok(())
@@ -14,8 +14,7 @@ pub struct CloseNetworkAccount<'info> {
         close = destination,
         seeds = [NETWORK_SEED, network.initial_authority.key().as_ref()],
         bump = network.signer_bump,
-        // TODO ??
-        // constraint = network.can_access(&mut authority, NetworkKeyFlags::AUTH),
+        constraint = network.can_access(&authority, NetworkKeyFlags::AUTH),
     )]
     pub network: Account<'info, GatekeeperNetwork>,
     /// CHECK: Rent destination account does not need to satisfy the any constraints.
