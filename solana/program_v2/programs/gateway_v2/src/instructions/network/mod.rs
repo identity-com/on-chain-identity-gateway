f1c57704392c0907dbf717dd62e2d07605b18783
DiosAkhilleus <abertin1809@gmail.com>
2022-09-20 15:30:53 -0500

    reformatted program structure

---
+++ solana/program_v2/programs/gateway_v2/src/instructions/network/mod.rs
@@ -0,0 +1,8 @@
+//! Network instructions for gateway v2.
+mod close_network;
+mod create_network;
+mod update_network;
+
+pub use close_network::*;
+pub use create_network::*;
+pub use update_network::*;
