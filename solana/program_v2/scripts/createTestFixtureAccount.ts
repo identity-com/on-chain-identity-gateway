a330d7b5527ba1771a51b8ffff785cad722f32d9
William Brooks <william@identity.org>
2022-09-26 19:18:35 +0200

    Added generating for gatekeeper for test fixture

--- solana/program_v2/scripts/createTestFixtureAccount.ts
+++ solana/program_v2/scripts/createTestFixtureAccount.ts
@@ -6,6 +6,7 @@ import {GatewayV2} from '../target/types/gateway_v2';
 import {airdrop} from '../src/lib/utils';
 import {GATEWAY_PROGRAM} from '../src/lib/constants';
 import {AdminService} from "../src/AdminService";
+import {NetworkService} from "../src/NetworkService";
 
 const exec = util.promisify(execCB);
 
@@ -13,77 +14,121 @@ const fixturePath = './tests/fixtures/accounts/';
 
 //copied from anchor
 export async function idlAddress(programId: PublicKey): Promise<PublicKey> {
-  const base = (await PublicKey.findProgramAddress([], programId))[0];
-  return await PublicKey.createWithSeed(base, 'anchor:idl', programId);
+    const base = (await PublicKey.findProgramAddress([], programId))[0];
+    return await PublicKey.createWithSeed(base, 'anchor:idl', programId);
 }
+
+anchor.setProvider(anchor.AnchorProvider.env());
+const program = anchor.workspace.GatewayV2 as anchor.Program<GatewayV2>;
+const programProvider = program.provider as anchor.AnchorProvider;
+
 
 const createNetworkAccount = async () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
-  const program = anchor.workspace.GatewayV2 as anchor.Program<GatewayV2>;
-  const programProvider = program.provider as anchor.AnchorProvider;
 
-  let authorityKeypair = Keypair.fromSecretKey(new Uint8Array(require(`../tests/fixtures/keypairs/B4951ZxztgHL98WT4eFUyaaRmsi6V4hBzkoYe1VSNweo.json`)));
-  let authority = new anchor.Wallet(authorityKeypair);
+    let authorityKeypair = Keypair.fromSecretKey(new Uint8Array(require(`../tests/fixtures/keypairs/B4951ZxztgHL98WT4eFUyaaRmsi6V4hBzkoYe1VSNweo.json`)));
+    let authority = new anchor.Wallet(authorityKeypair);
 
-  await airdrop(
-      programProvider.connection,
-      authority.publicKey,
-      LAMPORTS_PER_SOL * 2
-  );
+    await airdrop(
+        programProvider.connection,
+        authority.publicKey,
+        LAMPORTS_PER_SOL * 2
+    );
 
-  const [dataAccount] = await AdminService.createNetworkAddress(
-      authority.publicKey
-  );
+    const [dataAccount] = await AdminService.createNetworkAddress(
+        authority.publicKey
+    );
 
-  const service = await AdminService.buildFromAnchor(
-      program,
-      dataAccount,
-      'localnet',
-      programProvider,
-      authority
-  );
+    const service = await AdminService.buildFromAnchor(
+        program,
+        dataAccount,
+        'localnet',
+        programProvider,
+        authority
+    );
 
-  const foundAccount = await service.getNetworkAccount();
+    const foundAccount = await service.getNetworkAccount();
 
-  if (!foundAccount)
-    await service.createNetwork().rpc();
+    if (!foundAccount)
+        await service.createNetwork().rpc();
 
-  await exec(
-      `solana account ${dataAccount.toBase58()} -ul -o ${fixturePath}network-account.json --output json`
-  );
+    await exec(
+        `solana account ${dataAccount.toBase58()} -ul -o ${fixturePath}network-account.json --output json`
+    );
 
-  console.log("Test network account created");
+    console.log("Test network account created");
+
+    return dataAccount;
 }
 
+const createGatekeeperAccount = async (network: PublicKey) => {
+    anchor.setProvider(anchor.AnchorProvider.env());
+    const program = anchor.workspace.GatewayV2 as anchor.Program<GatewayV2>;
+    const programProvider = program.provider as anchor.AnchorProvider;
+
+    let authorityKeypair = Keypair.fromSecretKey(new Uint8Array(require(`../tests/fixtures/keypairs/B4951ZxztgHL98WT4eFUyaaRmsi6V4hBzkoYe1VSNweo.json`)));
+    let authority = new anchor.Wallet(authorityKeypair);
+
+    await airdrop(
+        programProvider.connection,
+        authority.publicKey,
+        LAMPORTS_PER_SOL * 2
+    );
+
+    const [dataAccount] = await NetworkService.createGatekeeperAddress(
+        authority.publicKey
+    );
+
+    const service = await NetworkService.buildFromAnchor(
+        program,
+        dataAccount,
+        'localnet',
+        programProvider,
+        authority
+    );
+
+    const foundAccount = await service.getGatekeeperAccount();
+
+    if (!foundAccount)
+        await service.createGatekeeper(network).rpc();
+
+    await exec(
+        `solana account ${dataAccount.toBase58()} -ul -o ${fixturePath}gatekeeper-account.json --output json`
+    );
+
+    console.log("Test network account created");
+}
+
+
 (async () => {
-  const program = anchor.workspace.GatewayV2 as anchor.Program<GatewayV2>;
-  const programProvider = program.provider as anchor.AnchorProvider;
+    const program = anchor.workspace.GatewayV2 as anchor.Program<GatewayV2>;
+    const programProvider = program.provider as anchor.AnchorProvider;
 
-  const authority = programProvider.wallet;
+    const authority = programProvider.wallet;
 
-  await airdrop(
-      programProvider.connection,
-      authority.publicKey,
-      100 * anchor.web3.LAMPORTS_PER_SOL
-  );
+    await airdrop(
+        programProvider.connection,
+        authority.publicKey,
+        100 * anchor.web3.LAMPORTS_PER_SOL
+    );
 
-  console.log(
-      `anchor idl init --filepath ./target/idl/gateway_v2.json ${GATEWAY_PROGRAM}`
-  );
-  console.log('Deploying IDL');
-  // Deploy IDL
-  await exec(
-      `anchor idl init --filepath ./target/idl/gateway_v2.json ${GATEWAY_PROGRAM}`
-  );
+    console.log(
+        `anchor idl init --filepath ./target/idl/gateway_v2.json ${GATEWAY_PROGRAM}`
+    );
+    console.log('Deploying IDL');
+    // Deploy IDL
+    await exec(
+        `anchor idl init --filepath ./target/idl/gateway_v2.json ${GATEWAY_PROGRAM}`
+    );
 
-  // write account
-  const idlAddr = await idlAddress(GATEWAY_PROGRAM);
-  console.log(`Done deploying IDL at ${idlAddr.toBase58()}`);
+    // write account
+    const idlAddr = await idlAddress(GATEWAY_PROGRAM);
+    console.log(`Done deploying IDL at ${idlAddr.toBase58()}`);
 
-  await exec(
-      `solana account ${idlAddr.toBase58()} -ul -o ${fixturePath}idl-account.json --output json`
-  );
+    await exec(
+        `solana account ${idlAddr.toBase58()} -ul -o ${fixturePath}idl-account.json --output json`
+    );
 
-  // Create the network account to be used in tests
-  await createNetworkAccount();
+    // Create the network account to be used in tests
+    const networkAccount = await createNetworkAccount();
+    const gatekeeperAccount = await createGatekeeperAccount(networkAccount);
 })().catch(console.error);
