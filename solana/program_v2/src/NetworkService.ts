9108434297028047ab71631b1e2ed2df69db468b
William Brooks <william@identity.org>
2022-10-05 15:16:00 +0200

    Linting fixes

--- solana/program_v2/src/NetworkService.ts
+++ solana/program_v2/src/NetworkService.ts
@@ -1,270 +1,270 @@
 import * as anchor from '@project-serum/anchor';
-import {AnchorProvider, Program} from '@project-serum/anchor';
-import {ConfirmOptions, PublicKey} from '@solana/web3.js';
+import { AnchorProvider, Program } from '@project-serum/anchor';
+import { ConfirmOptions, PublicKey } from '@solana/web3.js';
 import {
-    AuthKeyStructure,
-    CreateGatekeeperData,
-    UpdateGatekeeperData,
-    FeeStructure,
-    Wallet,
-    GatekeeperAccount,
-    GatekeeperState,
-    GatekeeperStateMapping,
+  AuthKeyStructure,
+  CreateGatekeeperData,
+  UpdateGatekeeperData,
+  FeeStructure,
+  Wallet,
+  GatekeeperAccount,
+  GatekeeperState,
+  GatekeeperStateMapping,
 } from './lib/types';
 import {
-    CustomClusterUrlConfig,
-    ExtendedCluster,
-    getConnectionByCluster,
+  CustomClusterUrlConfig,
+  ExtendedCluster,
+  getConnectionByCluster,
 } from './lib/connection';
-import {EnumMapper} from './lib/utils';
+import { EnumMapper } from './lib/utils';
 import {
-    GatekeeperKeyFlags,
-    GATEKEEPER_SEED,
-    GATEWAY_PROGRAM,
-    SOLANA_MAINNET,
+  GatekeeperKeyFlags,
+  GATEKEEPER_SEED,
+  GATEWAY_PROGRAM,
+  SOLANA_MAINNET,
 } from './lib/constants';
-import {AbstractService, ServiceBuilder} from './utils/AbstractService';
-import {GatewayV2} from '../target/types/gateway_v2';
+import { AbstractService, ServiceBuilder } from './utils/AbstractService';
+import { GatewayV2 } from '../target/types/gateway_v2';
 
 // Service for a network. This will handle all aspects of the Gateway that a network is able to control... i.e. creating gatekeepers, updating gatekeepers, etc...
 export class NetworkService extends AbstractService {
-    static async build(
-        dataAccount: PublicKey,
-        wallet: Wallet,
-        cluster: ExtendedCluster = SOLANA_MAINNET,
-        customConfig?: CustomClusterUrlConfig,
-        opts: ConfirmOptions = AnchorProvider.defaultOptions()
-    ): Promise<NetworkService> {
-        const _connection = getConnectionByCluster(
-            cluster,
-            opts.preflightCommitment,
-            customConfig
-        );
+  static async build(
+    dataAccount: PublicKey,
+    wallet: Wallet,
+    cluster: ExtendedCluster = SOLANA_MAINNET,
+    customConfig?: CustomClusterUrlConfig,
+    opts: ConfirmOptions = AnchorProvider.defaultOptions()
+  ): Promise<NetworkService> {
+    const _connection = getConnectionByCluster(
+      cluster,
+      opts.preflightCommitment,
+      customConfig
+    );
 
-        const provider = new AnchorProvider(_connection, wallet, opts);
+    const provider = new AnchorProvider(_connection, wallet, opts);
 
-        const program = await NetworkService.fetchProgram(provider);
+    const program = await NetworkService.fetchProgram(provider);
 
-        return new NetworkService(
-            program,
-            dataAccount,
-            cluster,
-            wallet,
-            provider.opts
-        );
-    }
+    return new NetworkService(
+      program,
+      dataAccount,
+      cluster,
+      wallet,
+      provider.opts
+    );
+  }
 
-    static async buildFromAnchor(
-        program: Program<GatewayV2>,
-        dataAccount: PublicKey,
-        cluster: ExtendedCluster,
-        provider: AnchorProvider = program.provider as AnchorProvider,
-        wallet: Wallet = provider.wallet
-    ): Promise<NetworkService> {
-        return new NetworkService(
-            program,
-            dataAccount,
-            cluster,
-            wallet,
-            provider.opts
-        );
-    }
+  static async buildFromAnchor(
+    program: Program<GatewayV2>,
+    dataAccount: PublicKey,
+    cluster: ExtendedCluster,
+    provider: AnchorProvider = program.provider as AnchorProvider,
+    wallet: Wallet = provider.wallet
+  ): Promise<NetworkService> {
+    return new NetworkService(
+      program,
+      dataAccount,
+      cluster,
+      wallet,
+      provider.opts
+    );
+  }
 
-    // Creates a gatekeeper's public key from a given seed and authority.
-    static async createGatekeeperAddress(
-        authority: PublicKey,
-        network: PublicKey
-    ): Promise<[PublicKey, number]> {
-        return PublicKey.findProgramAddress(
-            [
-                anchor.utils.bytes.utf8.encode(GATEKEEPER_SEED),
-                authority.toBuffer(),
-                network.toBuffer(),
-            ],
-            GATEWAY_PROGRAM
-        );
-    }
+  // Creates a gatekeeper's public key from a given seed and authority.
+  static async createGatekeeperAddress(
+    authority: PublicKey,
+    network: PublicKey
+  ): Promise<[PublicKey, number]> {
+    return PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode(GATEKEEPER_SEED),
+        authority.toBuffer(),
+        network.toBuffer(),
+      ],
+      GATEWAY_PROGRAM
+    );
+  }
 
-    // Creates a gatekeeper on a specified network
-    // @authThreshold
-    createGatekeeper(
-        network: PublicKey,
-        data: CreateGatekeeperData = {
-            gatekeeperBump: 0,
-            gatekeeperNetwork: network,
-            // TODO? Is this the correct way to derive the default staking account?
-            stakingAccount: PublicKey.findProgramAddressSync(
-                [
-                    anchor.utils.bytes.utf8.encode('gw-stake'),
-                    this._wallet.publicKey.toBuffer(),
-                ],
-                GATEWAY_PROGRAM
-            )[0],
-            tokenFees: [],
-            authThreshold: 1,
-            authKeys: [
-                {
-                    flags: GatekeeperKeyFlags.AUTH | GatekeeperKeyFlags.SET_EXPIRE_TIME,
-                    key: this._wallet.publicKey,
-                },
-            ],
+  // Creates a gatekeeper on a specified network
+  // @authThreshold
+  createGatekeeper(
+    network: PublicKey,
+    data: CreateGatekeeperData = {
+      gatekeeperBump: 0,
+      gatekeeperNetwork: network,
+      // TODO? Is this the correct way to derive the default staking account?
+      stakingAccount: PublicKey.findProgramAddressSync(
+        [
+          anchor.utils.bytes.utf8.encode('gw-stake'),
+          this._wallet.publicKey.toBuffer(),
+        ],
+        GATEWAY_PROGRAM
+      )[0],
+      tokenFees: [],
+      authThreshold: 1,
+      authKeys: [
+        {
+          flags: GatekeeperKeyFlags.AUTH | GatekeeperKeyFlags.SET_EXPIRE_TIME,
+          key: this._wallet.publicKey,
         },
-        authority: PublicKey = this._wallet.publicKey
-    ): ServiceBuilder {
-        const instructionPromise = this._program.methods
-            .createGatekeeper({
-                gatekeeperBump: data.gatekeeperBump,
-                gatekeeperNetwork: data.gatekeeperNetwork,
-                stakingAccount: data.stakingAccount,
-                tokenFees: data.tokenFees,
-                authThreshold: data.authThreshold,
-                authKeys: data.authKeys,
-            })
-            .accounts({
-                gatekeeper: this._dataAccount,
-                systemProgram: anchor.web3.SystemProgram.programId,
-                authority,
-                network
-            })
-            .instruction();
-        return new ServiceBuilder(this, {
-            instructionPromise,
-            didAccountSizeDeltaCallback: () => {
-                throw new Error('Dynamic Alloc not supported');
-            },
-            allowsDynamicAlloc: false,
-            authority,
-        });
-    }
+      ],
+    },
+    authority: PublicKey = this._wallet.publicKey
+  ): ServiceBuilder {
+    const instructionPromise = this._program.methods
+      .createGatekeeper({
+        gatekeeperBump: data.gatekeeperBump,
+        gatekeeperNetwork: data.gatekeeperNetwork,
+        stakingAccount: data.stakingAccount,
+        tokenFees: data.tokenFees,
+        authThreshold: data.authThreshold,
+        authKeys: data.authKeys,
+      })
+      .accounts({
+        gatekeeper: this._dataAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        authority,
+        network,
+      })
+      .instruction();
+    return new ServiceBuilder(this, {
+      instructionPromise,
+      didAccountSizeDeltaCallback: () => {
+        throw new Error('Dynamic Alloc not supported');
+      },
+      allowsDynamicAlloc: false,
+      authority,
+    });
+  }
 
-    // Allows a network to update a gatekeeper's data
-    updateGatekeeper(
-        data: UpdateGatekeeperData,
-        authority: PublicKey = this._wallet.publicKey
-    ): ServiceBuilder {
-        const instructionPromise = this._program.methods
-            // @ts-ignore
-            .updateGatekeeper({
-                authThreshold: data.authThreshold,
-                gatekeeperNetwork: data.gatekeeperNetwork,
-                stakingAccount: data.stakingAccount,
-                tokenFees: data.tokenFees,
-                authKeys: data.authKeys,
-            })
-            .accounts({
-                gatekeeper: this._dataAccount,
-                systemProgram: anchor.web3.SystemProgram.programId,
-                authority,
-            })
-            .instruction();
+  // Allows a network to update a gatekeeper's data
+  updateGatekeeper(
+    data: UpdateGatekeeperData,
+    authority: PublicKey = this._wallet.publicKey
+  ): ServiceBuilder {
+    const instructionPromise = this._program.methods
+      // @ts-ignore
+      .updateGatekeeper({
+        authThreshold: data.authThreshold,
+        gatekeeperNetwork: data.gatekeeperNetwork,
+        stakingAccount: data.stakingAccount,
+        tokenFees: data.tokenFees,
+        authKeys: data.authKeys,
+      })
+      .accounts({
+        gatekeeper: this._dataAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        authority,
+      })
+      .instruction();
 
-        return new ServiceBuilder(this, {
-            instructionPromise,
-            didAccountSizeDeltaCallback: () => {
-                throw new Error('Dynamic Alloc not supported');
-            },
-            allowsDynamicAlloc: false,
-            authority,
-        });
-    }
+    return new ServiceBuilder(this, {
+      instructionPromise,
+      didAccountSizeDeltaCallback: () => {
+        throw new Error('Dynamic Alloc not supported');
+      },
+      allowsDynamicAlloc: false,
+      authority,
+    });
+  }
 
-    // Closes a gatekeeper on a network
-    closeGatekeeper(
-        network: PublicKey,
-        destination: PublicKey = this._wallet.publicKey,
-        authority: PublicKey = this._wallet.publicKey
-    ): ServiceBuilder {
-        const instructionPromise = this._program.methods
-            // @ts-ignore
-            .closeGatekeeper()
-            .accounts({
-                gatekeeper: this._dataAccount,
-                systemProgram: anchor.web3.SystemProgram.programId,
-                destination,
-                authority,
-                network
-            })
-            .instruction();
+  // Closes a gatekeeper on a network
+  closeGatekeeper(
+    network: PublicKey,
+    destination: PublicKey = this._wallet.publicKey,
+    authority: PublicKey = this._wallet.publicKey
+  ): ServiceBuilder {
+    const instructionPromise = this._program.methods
+      // @ts-ignore
+      .closeGatekeeper()
+      .accounts({
+        gatekeeper: this._dataAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        destination,
+        authority,
+        network,
+      })
+      .instruction();
 
-        return new ServiceBuilder(this, {
-            instructionPromise,
-            didAccountSizeDeltaCallback: () => {
-                throw new Error('Dynamic Alloc not supported');
-            },
-            allowsDynamicAlloc: false,
-            authority,
-        });
-    }
+    return new ServiceBuilder(this, {
+      instructionPromise,
+      didAccountSizeDeltaCallback: () => {
+        throw new Error('Dynamic Alloc not supported');
+      },
+      allowsDynamicAlloc: false,
+      authority,
+    });
+  }
 
-    // Allows an authority to update the state of a gatekeeper (Active, Frozen, Halted)
-    setGatekeeperState(
-        state: GatekeeperState = GatekeeperState.Active,
-        authority: PublicKey = this._wallet.publicKey
-    ): ServiceBuilder {
-        const instructionPromise = this._program.methods
-            .setGatekeeperState(EnumMapper.to(state, GatekeeperStateMapping))
-            .accounts({
-                gatekeeper: this._dataAccount,
-                systemProgram: anchor.web3.SystemProgram.programId,
-                authority,
-            })
-            .instruction();
+  // Allows an authority to update the state of a gatekeeper (Active, Frozen, Halted)
+  setGatekeeperState(
+    state: GatekeeperState = GatekeeperState.Active,
+    authority: PublicKey = this._wallet.publicKey
+  ): ServiceBuilder {
+    const instructionPromise = this._program.methods
+      .setGatekeeperState(EnumMapper.to(state, GatekeeperStateMapping))
+      .accounts({
+        gatekeeper: this._dataAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        authority,
+      })
+      .instruction();
 
-        return new ServiceBuilder(this, {
-            instructionPromise,
-            didAccountSizeDeltaCallback: () => {
-                throw new Error('Dynamic Alloc not supported');
-            },
-            allowsDynamicAlloc: false,
-            authority,
-        });
-    }
+    return new ServiceBuilder(this, {
+      instructionPromise,
+      didAccountSizeDeltaCallback: () => {
+        throw new Error('Dynamic Alloc not supported');
+      },
+      allowsDynamicAlloc: false,
+      authority,
+    });
+  }
 
-    // Controls withdrawal of funds from a gatekeeper
-    gatekeeperWithdraw(
-        receiver: PublicKey = this._wallet.publicKey,
-        authority: PublicKey = this._wallet.publicKey
-    ): ServiceBuilder {
-        const instructionPromise = this._program.methods
-            .gatekeeperWithdraw(receiver)
-            .accounts({
-                gatekeeper: this._dataAccount,
-                systemProgram: anchor.web3.SystemProgram.programId,
-                authority,
-            })
-            .instruction();
+  // Controls withdrawal of funds from a gatekeeper
+  gatekeeperWithdraw(
+    receiver: PublicKey = this._wallet.publicKey,
+    authority: PublicKey = this._wallet.publicKey
+  ): ServiceBuilder {
+    const instructionPromise = this._program.methods
+      .gatekeeperWithdraw(receiver)
+      .accounts({
+        gatekeeper: this._dataAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        authority,
+      })
+      .instruction();
 
-        return new ServiceBuilder(this, {
-            instructionPromise,
-            didAccountSizeDeltaCallback: () => {
-                throw new Error('Dynamic Alloc not supported');
-            },
-            allowsDynamicAlloc: false,
-            authority,
-        });
-    }
+    return new ServiceBuilder(this, {
+      instructionPromise,
+      didAccountSizeDeltaCallback: () => {
+        throw new Error('Dynamic Alloc not supported');
+      },
+      allowsDynamicAlloc: false,
+      authority,
+    });
+  }
 
-    // Retrieves a gatekeeper's information
-    async getGatekeeperAccount(
-        account: PublicKey = this._dataAccount
-    ): Promise<GatekeeperAccount | null> {
-        const gatekeeperAccount = this._program.account.gatekeeper
-            .fetchNullable(account)
-            .then((acct) => {
-                if (acct) {
-                    return {
-                        version: acct?.version,
-                        authority: acct?.authority,
-                        gatekeeperNetwork: acct?.gatekeeperNetwork,
-                        stakingAccount: acct?.stakingAccount,
-                        tokenFees: acct?.tokenFees as FeeStructure[],
-                        authKeys: acct?.authKeys as AuthKeyStructure[],
-                        state: acct?.gatekeeperState as GatekeeperState,
-                    };
-                } else {
-                    return null;
-                }
-            });
-        return gatekeeperAccount;
-    }
+  // Retrieves a gatekeeper's information
+  async getGatekeeperAccount(
+    account: PublicKey = this._dataAccount
+  ): Promise<GatekeeperAccount | null> {
+    const gatekeeperAccount = this._program.account.gatekeeper
+      .fetchNullable(account)
+      .then((acct) => {
+        if (acct) {
+          return {
+            version: acct?.version,
+            authority: acct?.authority,
+            gatekeeperNetwork: acct?.gatekeeperNetwork,
+            stakingAccount: acct?.stakingAccount,
+            tokenFees: acct?.tokenFees as FeeStructure[],
+            authKeys: acct?.authKeys as AuthKeyStructure[],
+            state: acct?.gatekeeperState as GatekeeperState,
+          };
+        } else {
+          return null;
+        }
+      });
+    return gatekeeperAccount;
+  }
 }
