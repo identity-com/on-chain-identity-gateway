14452202406b8f8bbd1b936fa50aa3a220e3affd
William Brooks <william@identity.org>
2022-10-05 14:04:10 +0200

    Fix: Updated constant for the network seed

--- solana/program_v2/src/AdminService.ts
+++ solana/program_v2/src/AdminService.ts
@@ -1,208 +1,209 @@
-import { AnchorProvider, Program } from '@project-serum/anchor';
+import {AnchorProvider, Program} from '@project-serum/anchor';
 import * as anchor from '@project-serum/anchor';
-import { ConfirmOptions, PublicKey } from '@solana/web3.js';
+import {ConfirmOptions, PublicKey} from '@solana/web3.js';
 
 import {
-  AuthKeyStructure,
-  CreateNetworkData,
-  FeeStructure,
-  NetworkAccount,
-  UpdateNetworkData,
-  Wallet,
+    AuthKeyStructure,
+    CreateNetworkData,
+    FeeStructure,
+    NetworkAccount,
+    UpdateNetworkData,
+    Wallet,
 } from './lib/types';
 
 import {
-  CustomClusterUrlConfig,
-  ExtendedCluster,
-  getConnectionByCluster,
+    CustomClusterUrlConfig,
+    ExtendedCluster,
+    getConnectionByCluster,
 } from './lib/connection';
-import { findProgramAddress } from './lib/utils';
+import {findProgramAddress} from './lib/utils';
 import {
-  DEFAULT_SEED_STRING,
-  GATEWAY_PROGRAM,
-  SOLANA_MAINNET,
+    NETWORK_SEED,
+    GATEWAY_PROGRAM,
+    SOLANA_MAINNET,
 } from './lib/constants';
-import { GatewayV2, IDL } from '../target/types/gateway_v2';
-import { AbstractService, ServiceBuilder } from './utils/AbstractService';
+import {GatewayV2, IDL} from '../target/types/gateway_v2';
+import {AbstractService, ServiceBuilder} from './utils/AbstractService';
 
 export class AdminService extends AbstractService {
-  static async build(
-    dataAccount: PublicKey,
-    wallet: Wallet,
-    cluster: ExtendedCluster = SOLANA_MAINNET,
-    customConfig?: CustomClusterUrlConfig,
-    opts: ConfirmOptions = AnchorProvider.defaultOptions()
-  ): Promise<AdminService> {
-    const _connection = getConnectionByCluster(
-      cluster,
-      opts.preflightCommitment,
-      customConfig
-    );
+    static async build(
+        dataAccount: PublicKey,
+        wallet: Wallet,
+        cluster: ExtendedCluster = SOLANA_MAINNET,
+        customConfig?: CustomClusterUrlConfig,
+        opts: ConfirmOptions = AnchorProvider.defaultOptions()
+    ): Promise<AdminService> {
+        const _connection = getConnectionByCluster(
+            cluster,
+            opts.preflightCommitment,
+            customConfig
+        );
 
-    const provider = new AnchorProvider(_connection, wallet, opts);
+        const provider = new AnchorProvider(_connection, wallet, opts);
 
-    const program = await AdminService.fetchProgram(provider);
+        const program = await AdminService.fetchProgram(provider);
 
-    return new AdminService(
-      program,
-      dataAccount,
-      cluster,
-      wallet,
-      provider.opts
-    );
-  }
+        return new AdminService(
+            program,
+            dataAccount,
+            cluster,
+            wallet,
+            provider.opts
+        );
+    }
 
-  static async buildFromAnchor(
-    program: Program<GatewayV2>,
-    dataAccount: PublicKey,
-    cluster: ExtendedCluster,
-    provider: AnchorProvider = program.provider as AnchorProvider,
-    wallet: Wallet = provider.wallet
-  ): Promise<AdminService> {
-    return new AdminService(
-      program,
-      dataAccount,
-      cluster,
-      wallet,
-      provider.opts
-    );
-  }
+    static async buildFromAnchor(
+        program: Program<GatewayV2>,
+        dataAccount: PublicKey,
+        cluster: ExtendedCluster,
+        provider: AnchorProvider = program.provider as AnchorProvider,
+        wallet: Wallet = provider.wallet
+    ): Promise<AdminService> {
+        return new AdminService(
+            program,
+            dataAccount,
+            cluster,
+            wallet,
+            provider.opts
+        );
+    }
 
-  static async createNetworkAddress(
-    authority: PublicKey,
-    networkIndex: number = 0
-  ): Promise<[PublicKey, number]> {
-    const network_index_buffer = Buffer.alloc(2);
-    network_index_buffer.writeInt16LE(networkIndex);
+    static async createNetworkAddress(
+        authority: PublicKey,
+        networkIndex: number = 0
+    ): Promise<[PublicKey, number]> {
+        const network_index_buffer = Buffer.alloc(2);
+        network_index_buffer.writeInt16LE(networkIndex);
 
-    return PublicKey.findProgramAddress(
-      [
-        anchor.utils.bytes.utf8.encode(DEFAULT_SEED_STRING),
-        authority.toBuffer(),
-        network_index_buffer,
-      ],
-      GATEWAY_PROGRAM
-    );
-  }
+        return PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode(NETWORK_SEED),
+                authority.toBuffer(),
+                network_index_buffer,
+            ],
+            GATEWAY_PROGRAM
+        );
+    }
 
-  closeNetwork(
-    destination: PublicKey = this._wallet.publicKey,
-    authority: PublicKey = this._wallet.publicKey
-  ): ServiceBuilder {
-    const instructionPromise = this._program.methods
-      .closeNetwork()
-      .accounts({
-        network: this._dataAccount,
-        destination,
-        authority,
-      })
-      .instruction();
+    closeNetwork(
+        destination: PublicKey = this._wallet.publicKey,
+        authority: PublicKey = this._wallet.publicKey
+    ): ServiceBuilder {
+        const instructionPromise = this._program.methods
+            .closeNetwork()
+            .accounts({
+                network: this._dataAccount,
+                destination,
+                authority,
+            })
+            .instruction();
 
-    return new ServiceBuilder(this, {
-      instructionPromise,
-      didAccountSizeDeltaCallback: () => {
-        throw new Error('Dynamic Alloc not supported');
-      },
-      allowsDynamicAlloc: false,
-      authority,
-    });
-  }
-  createNetwork(
-    data: CreateNetworkData = {
-      authThreshold: 1,
-      passExpireTime: 16,
-      fees: [],
-      authKeys: [{ flags: 4097, key: this._wallet.publicKey }],
-      networkIndex: 0,
-      gatekeepers: [],
-      supportedTokens: [],
-    },
-    authority: PublicKey = this._wallet.publicKey
-  ): ServiceBuilder {
-    const instructionPromise = this._program.methods
-      .createNetwork({
-        authThreshold: data.authThreshold,
-        passExpireTime: new anchor.BN(data.passExpireTime),
-        fees: data.fees,
-        authKeys: data.authKeys,
-        networkIndex: data.networkIndex,
-        gatekeepers: data.gatekeepers,
-        supportedTokens: data.supportedTokens,
-      })
-      .accounts({
-        network: this._dataAccount,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        authority,
-      })
-      .instruction();
+        return new ServiceBuilder(this, {
+            instructionPromise,
+            didAccountSizeDeltaCallback: () => {
+                throw new Error('Dynamic Alloc not supported');
+            },
+            allowsDynamicAlloc: false,
+            authority,
+        });
+    }
 
-    return new ServiceBuilder(this, {
-      instructionPromise,
-      didAccountSizeDeltaCallback: () => {
-        throw new Error('Dynamic Alloc not supported');
-      },
-      // TODO: Implement this...
-      allowsDynamicAlloc: false,
-      authority,
-    });
-  }
+    createNetwork(
+        data: CreateNetworkData = {
+            authThreshold: 1,
+            passExpireTime: 16,
+            fees: [],
+            authKeys: [{flags: 4097, key: this._wallet.publicKey}],
+            networkIndex: 0,
+            gatekeepers: [],
+            supportedTokens: [],
+        },
+        authority: PublicKey = this._wallet.publicKey
+    ): ServiceBuilder {
+        const instructionPromise = this._program.methods
+            .createNetwork({
+                authThreshold: data.authThreshold,
+                passExpireTime: new anchor.BN(data.passExpireTime),
+                fees: data.fees,
+                authKeys: data.authKeys,
+                networkIndex: data.networkIndex,
+                gatekeepers: data.gatekeepers,
+                supportedTokens: data.supportedTokens,
+            })
+            .accounts({
+                network: this._dataAccount,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                authority,
+            })
+            .instruction();
 
-  updateNetwork(
-    data: UpdateNetworkData,
-    authority: PublicKey = this._wallet.publicKey
-  ): ServiceBuilder {
-    const instructionPromise = this._program.methods
-      // @ts-ignore
-      .updateNetwork({
-        authThreshold: data.authThreshold,
-        passExpireTime: new anchor.BN(data.passExpireTime),
-        fees: data.fees,
-        authKeys: data.authKeys,
-        gatekeepers: data.gatekeepers,
-        supportedTokens: data.supportedTokens,
-        networkFeatures: data.networkFeatures,
-      })
-      .accounts({
-        network: this._dataAccount,
-        authority,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .instruction();
+        return new ServiceBuilder(this, {
+            instructionPromise,
+            didAccountSizeDeltaCallback: () => {
+                throw new Error('Dynamic Alloc not supported');
+            },
+            // TODO: Implement this...
+            allowsDynamicAlloc: false,
+            authority,
+        });
+    }
 
-    return new ServiceBuilder(this, {
-      instructionPromise,
-      didAccountSizeDeltaCallback: () => {
-        throw new Error('Dynamic Alloc not supported');
-      },
-      allowsDynamicAlloc: false,
-      authority,
-    });
-  }
+    updateNetwork(
+        data: UpdateNetworkData,
+        authority: PublicKey = this._wallet.publicKey
+    ): ServiceBuilder {
+        const instructionPromise = this._program.methods
+            // @ts-ignore
+            .updateNetwork({
+                authThreshold: data.authThreshold,
+                passExpireTime: new anchor.BN(data.passExpireTime),
+                fees: data.fees,
+                authKeys: data.authKeys,
+                gatekeepers: data.gatekeepers,
+                supportedTokens: data.supportedTokens,
+                networkFeatures: data.networkFeatures,
+            })
+            .accounts({
+                network: this._dataAccount,
+                authority,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            })
+            .instruction();
 
-  async getNetworkAccount(
-    account: PublicKey = this._dataAccount
-  ): Promise<NetworkAccount | null> {
-    const networkAccount = this._program.account.gatekeeperNetwork
-      .fetchNullable(account)
-      .then((acct) => {
-        if (acct) {
-          return {
-            version: acct?.version,
-            authority: acct?.authority,
-            networkIndex: acct?.networkIndex,
-            authThreshold: acct?.authThreshold,
-            passExpireTime: acct?.passExpireTime.toNumber(),
-            fees: acct?.fees as FeeStructure[],
-            authKeys: acct?.authKeys as AuthKeyStructure[],
-            networkFeatures: acct?.networkFeatures,
-            supportedTokens: acct?.supportedTokens,
-            gatekeepers: acct?.gatekeepers,
-          };
-        } else {
-          return null;
-        }
-      });
-    //@ts-ignore
-    return networkAccount;
-  }
+        return new ServiceBuilder(this, {
+            instructionPromise,
+            didAccountSizeDeltaCallback: () => {
+                throw new Error('Dynamic Alloc not supported');
+            },
+            allowsDynamicAlloc: false,
+            authority,
+        });
+    }
+
+    async getNetworkAccount(
+        account: PublicKey = this._dataAccount
+    ): Promise<NetworkAccount | null> {
+        const networkAccount = this._program.account.gatekeeperNetwork
+            .fetchNullable(account)
+            .then((acct) => {
+                if (acct) {
+                    return {
+                        version: acct?.version,
+                        authority: acct?.authority,
+                        networkIndex: acct?.networkIndex,
+                        authThreshold: acct?.authThreshold,
+                        passExpireTime: acct?.passExpireTime.toNumber(),
+                        fees: acct?.fees as FeeStructure[],
+                        authKeys: acct?.authKeys as AuthKeyStructure[],
+                        networkFeatures: acct?.networkFeatures,
+                        supportedTokens: acct?.supportedTokens,
+                        gatekeepers: acct?.gatekeepers,
+                    };
+                } else {
+                    return null;
+                }
+            });
+        //@ts-ignore
+        return networkAccount;
+    }
 }
