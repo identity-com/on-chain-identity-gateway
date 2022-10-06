2a9a7e0cc4e10c07593bfdf63feaf7ad3a670ecb
William Brooks <william@identity.org>
2022-10-06 12:11:58 +0200

    Fixed test fixture

--- solana/program_v2/tests/util/constants.ts
+++ solana/program_v2/tests/util/constants.ts
@@ -2,7 +2,7 @@ import {
     PublicKey,
 } from '@solana/web3.js';
 
-export const TEST_NETWORK = new PublicKey("8PyTqDcyDKbMQB99D2aoNLsBqgheQU3UV4xz28ZXJYmN");
+export const TEST_NETWORK = new PublicKey("F75rU4fRqxiqG6gJCjkqaPHAARbmc276Y6ENrCTLPs6G");
 export const TEST_ALT_NETWORK = new PublicKey("bpQveAfQRjDR8chgF8WdULBFU2EnDNcTT66te4nydwU");
 export const TEST_GATEKEEPER = new PublicKey("DrbTzfYLsrWbQ1ioC69L2VMBfuFeDjnYH9DoWPZn5qZA");
 export const TEST_MINT = new PublicKey("wLYV8imcPhPDZ3JJvUgSWv2p6PNz4RfFtveqn4esJGX");
