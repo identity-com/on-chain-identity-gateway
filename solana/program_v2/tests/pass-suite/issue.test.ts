2c44616438b65aa6c26b3bcac8b7cf55dda61e00
Martin Riedel <web@riedel-it.de>
2022-10-05 20:52:55 -0400

    chore: fixed test

--- solana/program_v2/tests/pass-suite/issue.test.ts
+++ solana/program_v2/tests/pass-suite/issue.test.ts
@@ -1,10 +1,7 @@
 import {GatekeeperService} from "../../src/GatekeeperService";
 import {PassState} from "../../src/lib/wrappers";
 import {TEST_GATEKEEPER, TEST_NETWORK} from "../util/constants";
-import * as anchor from "@project-serum/anchor";
 import chai from 'chai';
-import {AnchorProvider, Program, Wallet} from "@project-serum/anchor";
-import {GatewayV2} from '../../target/types/gateway_v2'
 import {Keypair} from "@solana/web3.js";
 import {createGatekeeperService} from "./util";
 
@@ -21,6 +18,7 @@ describe.only("Issue pass", () => {
         const subject = Keypair.generate().publicKey;
 
         const account = await GatekeeperService.createPassAddress(subject, TEST_NETWORK);
+        // expect(service.getNetwork().toBase58()).to.equal("8PyTqDcyDKbMQB99D2aoNLsBqgheQU3UV4xz28ZXJYmN");
         await service.issue(account, subject).rpc();
         const pass = await service.getPassAccount(subject);
 
