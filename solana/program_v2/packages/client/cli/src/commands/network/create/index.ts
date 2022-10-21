4026e4bd0f86835b7ed1d4ee92aec6eda84d74a9
DiosAkhilleus <abertin1809@gmail.com>
2022-10-21 14:24:28 -0500

    progress on CLI

--- solana/program_v2/packages/client/cli/src/commands/network/create/index.ts
+++ solana/program_v2/packages/client/cli/src/commands/network/create/index.ts
@@ -1,29 +1,28 @@
 import { Command, Flags } from '@oclif/core';
-// import { Wallet } from '@project-serum/anchor';
-// import { Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
-// TODO: Import Gateway Service properly with package.json
-// import { GatewayService } from 'solana/program_v2/src/AdminService';
-// import { airdrop } from '@identity.com/solana-gateway-ts-v2/src/lib/utils';
+import { Wallet } from '@project-serum/anchor';
+import { Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { AdminService, airdrop } from '@identity.com/gateway-solana-client';
+import fsPromises from 'node:fs/promises';
 export default class Create extends Command {
   static description = 'Creates a gatekeeper network';
 
   static examples = [
-    `$ gateway network create --data ./network.json --key ./funder-key.json --cluster localnet
+    `$ gateway network create --address ./network.json --funder ./funder-keypair.json --cluster localnet
 Latest Blockhash: [blockhash]
 `,
   ];
 
   static flags = {
     help: Flags.help({ char: 'h' }),
-    data: Flags.string({
-      char: 'd',
+    address: Flags.string({
+      char: 'a',
       description: 'Path to network data',
       required: false,
     }),
-    key: Flags.string({
-      char: 'k',
+    funder: Flags.string({
+      char: 'f',
       description: 'Path to a solana keypair',
-      required: true,
+      required: false,
     }),
     cluster: Flags.string({
       char: 'c',
@@ -36,32 +35,39 @@ Latest Blockhash: [blockhash]
 
   async run(): Promise<void> {
     this.log('run');
-    // const { flags } = await this.parse(Create);
-    // const programId = Keypair.generate().publicKey;
-    // // eslint-disable-next-line unicorn/prefer-module
-    // const localSecretKey = require(flags.key);
-    // const funder = localSecretKey
-    //   ? Keypair.fromSecretKey(Buffer.from(localSecretKey))
-    //   : Keypair.generate();
+    const { flags } = await this.parse(Create);
+    const localSecretKey = flags.funder
+      ? await fsPromises.readFile(`${__dirname}/${flags.funder}`)
+      : null;
 
-    // const [network] = await GatewayService.createNetworkAddress(programId);
-    // this.log(`${network}`);
-    // const gatewayService = await GatewayService.build(
-    //   network,
-    //   new Wallet(funder),
-    //   // TODO: mainnet is default
-    //   flags.cluster ? flags.cluster : 'localnet'
-    // );
-    // // TODO: Remove airdrop
-    // this.log('before airdrop');
-    // await airdrop(
-    //   gatewayService.getConnection(),
-    //   funder.publicKey,
-    //   LAMPORTS_PER_SOL
-    // );
-    // this.log('after airdrop');
+    const privateKey = localSecretKey
+      ? Uint8Array.from(JSON.parse(localSecretKey.toString()))
+      : null;
+    const authorityKeypair = privateKey
+      ? Keypair.fromSecretKey(privateKey)
+      : Keypair.generate();
 
-    // const createdNetworkSignature = await gatewayService.createNetwork().rpc();
-    // this.log(`--${createdNetworkSignature}`);
+    const authority = new Wallet(authorityKeypair);
+    this.log(`Admin Authority: ${authority.publicKey.toBase58()}`);
+
+    const [dataAccount] = await AdminService.createNetworkAddress(
+      authority.publicKey,
+      0
+    );
+    this.log(`Network Address: ${dataAccount}`);
+    const adminService = await AdminService.build(
+      dataAccount,
+      authority,
+      'localnet'
+    );
+    await airdrop(
+      adminService.getConnection(),
+      authority.publicKey,
+      LAMPORTS_PER_SOL * 2
+    );
+
+    const networkSignature = await adminService.createNetwork().rpc();
+
+    this.log(`Network Signature: ${networkSignature}`);
   }
 }
