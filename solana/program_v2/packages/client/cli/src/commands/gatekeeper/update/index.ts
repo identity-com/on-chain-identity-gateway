0f8499818bd042dc0924be7595054bd74391e39a
luke song <hyeonminsong7@gmail.com>
2022-10-28 01:07:53 -0700

    WIP:update_gatekeeper_CLI

--- solana/program_v2/packages/client/cli/src/commands/gatekeeper/update/index.ts
+++ solana/program_v2/packages/client/cli/src/commands/gatekeeper/update/index.ts
@@ -1,4 +1,14 @@
 import { Command, Flags } from '@oclif/core';
+import { parseGatekeeperUpdateData } from '../../../util/util';
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+import {
+  airdrop,
+  UpdateGatekeeperData,
+  NetworkService,
+} from '@identity.com/gateway-solana-client';
+import { Wallet } from '@project-serum/anchor';
+import fsPromises from 'node:fs/promises';
+import { ExtendedCluster } from '@identity.com/gateway-solana-client/dist/lib/connection';
 
 export default class Update extends Command {
   static description = 'Updates a gatekeeper on an existing network';
@@ -10,6 +20,17 @@ export default class Update extends Command {
 
   static flags = {
     help: Flags.help({ char: 'h' }),
+    gatekeeper: Flags.string({
+      char: 'g',
+      description: "String representing the gatekeeper's address",
+      required: true,
+    }),
+    data: Flags.string({
+      char: 'd',
+      description:
+        'Path to a JSON data file representing the new state of the network',
+      required: true,
+    }),
     network: Flags.string({
       char: 'n',
       description: "String representing the network's address",
@@ -27,6 +48,55 @@ export default class Update extends Command {
   async run(): Promise<void> {
     const { flags } = await this.parse(Update);
 
-    this.log(`network: ${flags.network}`);
+    const gatekeeper = new PublicKey(flags.gatekeeper);
+    const data = await fsPromises.readFile(`${__dirname}/${flags.data}`);
+    const updateData = JSON.parse(data.toString()) as UpdateGatekeeperData;
+    const stakingAccount = Keypair.generate().publicKey;
+
+    const localSecretKey = flags.funder
+      ? await fsPromises.readFile(`${__dirname}/${flags.funder}`)
+      : await fsPromises.readFile(
+          `${__dirname}/../../../keypairs/network-authority.json`
+        );
+
+    const authKey = await fsPromises.readFile(
+      `${__dirname}/../../../keypairs/gatekeeper-authority.json`
+    );
+
+    const authKeyArr = Uint8Array.from(JSON.parse(authKey.toString()));
+    const authPair = Keypair.fromSecretKey(authKeyArr);
+
+    const privateKey = Uint8Array.from(JSON.parse(localSecretKey.toString()));
+    const authorityKeypair = Keypair.fromSecretKey(privateKey);
+    const authorityWallet = new Wallet(authorityKeypair);
+
+    const networkService = await NetworkService.build(
+      authPair.publicKey,
+      gatekeeper,
+      {
+        wallet: authorityWallet,
+        clusterType: 'localnet' as ExtendedCluster,
+      }
+    );
+
+    await airdrop(
+      networkService.getConnection(),
+      authorityWallet.publicKey,
+      LAMPORTS_PER_SOL * 2
+    );
+
+    const parsedData = parseGatekeeperUpdateData(updateData);
+
+    const updateGatekeeperSignature = await networkService
+      .updateGatekeeper(parsedData, stakingAccount) // data.json, stakingAccount and autority key?
+      .rpc();
+
+    this.log(updateGatekeeperSignature);
+
+    const UpdatedGatekeeper = await networkService.getGatekeeperAccount(
+      gatekeeper
+    );
+
+    this.log(UpdatedGatekeeper?.tokenFees[0].token.toBase58());
   }
 }
