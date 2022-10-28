1a050c23082cdbd047d6f4a973d533664a3bf9a3
luke song <hyeonminsong7@gmail.com>
2022-10-28 01:07:53 -0700

    WIP:update_gatekeeper_CLI

--- solana/program_v2/packages/client/cli/src/util/util.ts
+++ solana/program_v2/packages/client/cli/src/util/util.ts
@@ -2,6 +2,7 @@ import {
   AuthKeyStructure,
   FeeStructure,
   SupportedToken,
+  UpdateGatekeeperData,
   UpdateNetworkData,
 } from '@identity.com/gateway-solana-client';
 import { PublicKey } from '@solana/web3.js';
@@ -34,6 +35,32 @@ export const parseNetworkUpdateData = (
         return token;
       });
       updateData[key].remove.map((token: PublicKey) => new PublicKey(token));
+    }
+  });
+  return updateData;
+};
+
+export const parseGatekeeperUpdateData = (
+  updateData: UpdateGatekeeperData
+): UpdateGatekeeperData => {
+  const keysArr = Object.keys(updateData);
+  // eslint-disable-next-line unicorn/no-array-for-each
+  keysArr.forEach((key) => {
+    if (key === 'fees') {
+      updateData[key].add.map((fee: FeeStructure) => {
+        fee.token = new PublicKey(fee.token);
+        return fee;
+      });
+      updateData[key].remove.map((fee: PublicKey) => new PublicKey(fee));
+    }
+    if (key === 'authKeys') {
+      updateData[key].add.map((authKey: AuthKeyStructure) => {
+        authKey.key = new PublicKey(authKey.key);
+        return authKey;
+      });
+      updateData[key].remove.map(
+        (authKey: PublicKey) => new PublicKey(authKey)
+      );
     }
   });
   return updateData;
