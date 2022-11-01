3cddbaacee59c21d381bf49ebbdd5ba5e05b253d
DiosAkhilleus <abertin1809@gmail.com>
2022-11-01 17:36:35 +0000

    modified verification command

--- solana/program_v2/packages/client/cli/src/commands/pass/verify.ts
+++ solana/program_v2/packages/client/cli/src/commands/pass/verify.ts
@@ -1,5 +1,5 @@
 // import { GatekeeperService } from '@identity.com/gateway-solana-client';
-import { findGatewayTokenByAccount } from '@identity.com/gateway-solana-client';
+import { findGatewayToken } from '@identity.com/gateway-solana-client';
 import {
   ExtendedCluster,
   getConnectionByCluster,
@@ -11,15 +11,15 @@ export default class Verify extends Command {
   static description = 'Verifies a gateway pass';
 
   static examples = [
-    `$ gateway pass verify --pass [address] --network [address] --keypair [path to keypair] --cluster [cluster type]
+    `$ gateway pass verify --subject [address] --network [address] --keypair [path to keypair] --cluster [cluster type]
 `,
   ];
 
   static flags = {
     help: Flags.help({ char: 'h' }),
-    pass: Flags.string({
+    subject: Flags.string({
       char: 's',
-      description: 'The address of the issued pass',
+      description: 'The address to check for a gateway pass',
       required: true,
     }),
     network: Flags.string({
@@ -39,7 +39,7 @@ export default class Verify extends Command {
   async run(): Promise<void> {
     const { flags } = await this.parse(Verify);
 
-    const pass = new PublicKey(flags.pass);
+    const subject = new PublicKey(flags.subject);
     const network = new PublicKey(flags.network);
     const cluster =
       flags.cluster === 'localnet' ||
@@ -50,9 +50,9 @@ export default class Verify extends Command {
         ? flags.cluster
         : 'localnet';
     const connection = getConnectionByCluster(cluster as ExtendedCluster);
-    const acct = await findGatewayTokenByAccount(connection, network, pass);
+    const acct = await findGatewayToken(connection, network, subject);
     if (!acct) {
-      return this.error('Invalid pass account');
+      return this.log('No gateway pass associated with subject');
     }
     this.log(`Pass Verified — Current Pass State: ${acct.state}`);
   }
