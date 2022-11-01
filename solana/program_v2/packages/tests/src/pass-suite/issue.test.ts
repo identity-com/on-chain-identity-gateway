af7db1b2468e6e44eb6fcad589be05791c461faa
DiosAkhilleus <abertin1809@gmail.com>
2022-11-01 17:02:05 +0000

    added test for new util

--- solana/program_v2/packages/tests/src/pass-suite/issue.test.ts
+++ solana/program_v2/packages/tests/src/pass-suite/issue.test.ts
@@ -4,6 +4,7 @@ import {
   PassState,
   onGatewayToken,
   findGatewayToken,
+  findGatewayTokenByAccount,
 } from '@identity.com/gateway-solana-client';
 import { TEST_GATEKEEPER, TEST_NETWORK } from '../util/constants';
 import chai from 'chai';
@@ -100,5 +101,21 @@ describe('Issue pass', () => {
     // CHECK: that the issueTime is recent (is this best?)
     expect(pass?.issueTime).to.be.greaterThan(new Date().getTime() - 5000);
     expect(pass?.issueTime).to.be.lessThan(new Date().getTime() + 5000);
+  });
+  it('Finds a gateway token after issue with verification util', async () => {
+    const subject = Keypair.generate().publicKey;
+
+    const account = await GatekeeperService.createPassAddress(
+      subject,
+      TEST_NETWORK
+    );
+
+    await service.issue(account, subject).rpc();
+    const pass = await findGatewayTokenByAccount(
+      service.getConnection(),
+      TEST_NETWORK,
+      account
+    );
+    expect(pass?.state).to.equal(0);
   });
 });
