e3f8b687af131f859412cd4b44cfe483a18639f4
William Brooks <william@identity.org>
2022-11-02 09:27:36 +0000

    Export program id

--- solana/program_v2/packages/client/core/src/index.ts
+++ solana/program_v2/packages/client/core/src/index.ts
@@ -1,7 +1,11 @@
 export { AdminService } from './AdminService';
 export { NetworkService } from './NetworkService';
 export { GatekeeperService } from './GatekeeperService';
-export { NetworkKeyFlags, GatekeeperKeyFlags } from './lib/constants';
+export {
+  NetworkKeyFlags,
+  GatekeeperKeyFlags,
+  GATEWAY_PROGRAM as GATEWAY_PROGRAM_ID,
+} from './lib/constants';
 export * from './lib/wrappers';
 export * from './lib/utils';
 export * from './lib/types';
