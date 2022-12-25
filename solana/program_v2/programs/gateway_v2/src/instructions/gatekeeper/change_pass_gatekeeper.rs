2c8bdfe5a347314c156abe8ddf6724a59a0b240b
William Brooks <william@identity.org>
2022-12-25 10:32:05 +0200

    Cleaned up constraint checks

--- solana/program_v2/programs/gateway_v2/src/instructions/gatekeeper/change_pass_gatekeeper.rs
+++ solana/program_v2/programs/gateway_v2/src/instructions/gatekeeper/change_pass_gatekeeper.rs
@@ -1,22 +1,13 @@
+use anchor_lang::prelude::*;
+
 use crate::constants::PASS_SEED;
 use crate::errors::PassErrors;
 use crate::state::{Gatekeeper, GatekeeperKeyFlags, GatekeeperNetwork, Pass};
-use anchor_lang::prelude::*;
 
 pub fn change_pass_gatekeeper(ctx: Context<PassChangeGatekeeper>) -> Result<()> {
     // TODO: Check if feature flag is set
     let pass = &mut ctx.accounts.pass;
-    let old_gatekeeper = &mut ctx.accounts.old_gatekeeper;
-    let new_gatekeeper = &mut ctx.accounts.new_gatekeeper;
-
-    require!(
-        pass.gatekeeper == old_gatekeeper.key(),
-        PassErrors::InvalidGatekeeper
-    );
-    require!(
-        old_gatekeeper.gatekeeper_network == new_gatekeeper.gatekeeper_network,
-        PassErrors::InvalidNetwork
-    );
+    let new_gatekeeper = &ctx.accounts.new_gatekeeper;
 
     pass.gatekeeper = new_gatekeeper.key();
 
@@ -26,15 +17,20 @@ pub fn change_pass_gatekeeper(ctx: Context<PassChangeGatekeeper>) -> Result<()> 
 #[derive(Accounts, Debug)]
 pub struct PassChangeGatekeeper<'info> {
     #[account(
-    seeds = [PASS_SEED, pass.subject.as_ref(), pass.network.key().as_ref(), &pass.pass_number.to_le_bytes() ],
-    bump,
-    constraint = old_gatekeeper.can_access(&authority, GatekeeperKeyFlags::CHANGE_PASS_GATEKEEPER),
+    seeds = [PASS_SEED, pass.subject.as_ref(), pass.network.key().as_ref(), & pass.pass_number.to_le_bytes() ],
+    bump = pass.signer_bump,
+    constraint = old_gatekeeper.can_access(& authority, GatekeeperKeyFlags::CHANGE_PASS_GATEKEEPER),
+    constraint = pass.gatekeeper == old_gatekeeper.key() @ PassErrors::InvalidGatekeeper,
     mut
     )]
     pub pass: Account<'info, Pass>,
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
     pub network: Account<'info, GatekeeperNetwork>,
+
     pub old_gatekeeper: Account<'info, Gatekeeper>,
+    #[account(
+    constraint = old_gatekeeper.gatekeeper_network == new_gatekeeper.gatekeeper_network @ PassErrors::InvalidNetwork
+    )]
     pub new_gatekeeper: Account<'info, Gatekeeper>,
 }
