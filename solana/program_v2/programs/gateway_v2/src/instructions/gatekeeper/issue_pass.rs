3726b887581f50140d0aeb6a4009418e43266ab0
William Brooks <william@identity.org>
2023-01-19 13:38:51 +0200

    added a constraint to check the issuing gatekeeper is correct

--- solana/program_v2/programs/gateway_v2/src/instructions/gatekeeper/issue_pass.rs
+++ solana/program_v2/programs/gateway_v2/src/instructions/gatekeeper/issue_pass.rs
@@ -77,6 +77,7 @@ pub struct IssuePass<'info> {
     pub network: Box<Account<'info, GatekeeperNetwork>>,
     #[account(
     seeds = [GATEKEEPER_SEED, gatekeeper.authority.as_ref(), network.key().as_ref()],
+    constraint = pass.gatekeeper == gatekeeper.key(),
     bump = gatekeeper.gatekeeper_bump
     )]
     pub gatekeeper: Box<Account<'info, Gatekeeper>>,
@@ -86,7 +87,7 @@ pub struct IssuePass<'info> {
     pub funder: Signer<'info>,
     pub system_program: Program<'info, System>,
     pub spl_token_program: Program<'info, Token>,
-    #[account(constraint = network.is_token_supported(&mint_account.key()) @ NetworkErrors::TokenNotSupported)]
+    #[account(constraint = network.is_token_supported(& mint_account.key()) @ NetworkErrors::TokenNotSupported)]
     pub mint_account: Account<'info, Mint>,
     #[account(mut)]
     pub funder_token_account: Account<'info, TokenAccount>,
