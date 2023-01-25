f360c6ec54004516334e323d1f3e64d67b58edbe
William Brooks <william@identity.org>
2023-01-25 17:45:29 +0200

    fix: linting

--- solana/program_v2/packages/client/cli/src/commands/network/create.ts
+++ solana/program_v2/packages/client/cli/src/commands/network/create.ts
@@ -1,77 +1,81 @@
-import {Command, Flags} from '@oclif/core';
-import {Wallet} from '@project-serum/anchor';
-import {Keypair} from '@solana/web3.js';
-import {AdminService, ExtendedCluster, NetworkKeyFlags,} from '@identity.com/gateway-solana-client';
+import { Command, Flags } from '@oclif/core';
+import { Wallet } from '@project-serum/anchor';
+import { Keypair } from '@solana/web3.js';
+import {
+  AdminService,
+  ExtendedCluster,
+  NetworkKeyFlags,
+} from '@identity.com/gateway-solana-client';
 import fsPromises from 'node:fs/promises';
-import {NetworkFeatures} from "@identity.com/gateway-solana-client/dist/lib/constants";
+import { NetworkFeatures } from '@identity.com/gateway-solana-client/dist/lib/constants';
 
 export default class Create extends Command {
-    static description = 'Creates a gatekeeper network';
+  static description = 'Creates a gatekeeper network';
 
-    static examples = [
-        `$ gateway network create --keypair [path to keypair] --index [network index] --cluster [cluster type]
+  static examples = [
+    `$ gateway network create --keypair [path to keypair] --index [network index] --cluster [cluster type]
 `,
-    ];
+  ];
 
-    static flags = {
-        help: Flags.help({char: 'h'}),
-        keypair: Flags.string({
-            char: 'k',
-            description: 'Path to a Solana keypair',
-            required: true,
-        }),
-        cluster: Flags.string({
-            char: 'c',
-            description: 'The cluster you wish to use',
-            required: true,
-        }),
+  static flags = {
+    help: Flags.help({ char: 'h' }),
+    keypair: Flags.string({
+      char: 'k',
+      description: 'Path to a Solana keypair',
+      required: true,
+    }),
+    cluster: Flags.string({
+      char: 'c',
+      description: 'The cluster you wish to use',
+      required: true,
+    }),
+  };
+
+  static args = [];
+
+  async run(): Promise<void> {
+    const { flags } = await this.parse(Create);
+    const cluster =
+      flags.cluster === 'localnet' ||
+      flags.cluster === 'devnet' ||
+      flags.cluster === 'mainnet' ||
+      flags.cluster === 'civicnet' ||
+      flags.cluster === 'testnet'
+        ? flags.cluster
+        : 'localnet';
+    const localSecretKey = await fsPromises.readFile(`${flags.auth}`);
+    const privateKey = Uint8Array.from(JSON.parse(localSecretKey.toString()));
+    const authorityKeypair = Keypair.fromSecretKey(privateKey);
+    const authority = new Wallet(authorityKeypair);
+    this.log(`Admin Authority: ${authority.publicKey.toBase58()}`);
+
+    const adminService = await AdminService.build(authorityKeypair.publicKey, {
+      wallet: authority,
+      clusterType: cluster as ExtendedCluster,
+    });
+
+    const networkData = {
+      authThreshold: 1,
+      passExpireTime: 16,
+      fees: [],
+      authKeys: [
+        {
+          flags:
+            NetworkKeyFlags.AUTH |
+            NetworkKeyFlags.ADD_FEES |
+            NetworkKeyFlags.ADJUST_FEES |
+            NetworkKeyFlags.SET_EXPIRE_TIME,
+          key: authority.publicKey,
+        },
+      ],
+      gatekeepers: [],
+      supportedTokens: [],
+      networkFeatures: NetworkFeatures.CHANGE_PASS_GATEKEEPER,
     };
+    const networkSignature = await adminService
+      .createNetwork(networkData)
+      .rpc();
 
-    static args = [];
-
-    async run(): Promise<void> {
-        const {flags} = await this.parse(Create);
-        const cluster =
-            flags.cluster === 'localnet' ||
-            flags.cluster === 'devnet' ||
-            flags.cluster === 'mainnet' ||
-            flags.cluster === 'civicnet' ||
-            flags.cluster === 'testnet'
-                ? flags.cluster
-                : 'localnet';
-        const localSecretKey = await fsPromises.readFile(`${flags.auth}`);
-        const privateKey = Uint8Array.from(JSON.parse(localSecretKey.toString()));
-        const authorityKeypair = Keypair.fromSecretKey(privateKey);
-        const authority = new Wallet(authorityKeypair);
-        this.log(`Admin Authority: ${authority.publicKey.toBase58()}`);
-
-        const adminService = await AdminService.build(authorityKeypair.publicKey, {
-            wallet: authority,
-            clusterType: cluster as ExtendedCluster,
-        });
-
-        const networkData = {
-            authThreshold: 1,
-            passExpireTime: 16,
-            fees: [],
-            authKeys: [
-                {
-                    flags:
-                        NetworkKeyFlags.AUTH |
-                        NetworkKeyFlags.ADD_FEES |
-                        NetworkKeyFlags.ADJUST_FEES |
-                        NetworkKeyFlags.SET_EXPIRE_TIME,
-                    key: authority.publicKey,
-                },
-            ],
-            gatekeepers: [],
-            supportedTokens: [],
-            networkFeatures: NetworkFeatures.CHANGE_PASS_GATEKEEPER
-        };
-        const networkSignature = await adminService
-            .createNetwork(networkData)
-            .rpc();
-
-        this.log(`Network Signature: ${networkSignature}`);
-    }
+    this.log(`Network Signature: ${networkSignature}`);
+  }
 }
