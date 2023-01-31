d40426f380c4d96d01ddec78fa24d3c44a55fe9b
Tighe Barris <4658684+tbarri@users.noreply.github.com>
2023-01-31 15:15:36 +0200

    gateway_v2: Remove dead testing code from the CLI
    This change removed unused code and stubs from the CLI package.
    
    PR: https://github.com/identity-com/on-chain-identity-gateway/pull/374
    
    Reviewed-by: Martin Riedel <web@riedel-it.de>
    Reviewed-by: Julian Spring <springjulian@gmail.com>
    Reviewed-by: William Brooks <william@identity.org>
    Signed-off-by: Tighe Barris <4658684+tbarri@users.noreply.github.com>

--- solana/program_v2/packages/client/cli/test/commands/network/create.test.ts
+++
@@ -1,8 +0,0 @@
-// import { expect, test } from "@oclif/test";
-// import { describe } from 'mocha';
-
-// describe('creates a network', () => {
-//   it('Runs the network create test', async function () {
-//     console.log('gateway network create');
-//   });
-// });
