3fb588e988cbe9b558e9c6db13aa0e4765614116
dankelleher <dankelleher@yahoo.com>
2023-04-17 11:56:52 +0200

    Eth Smart Contract audit: Rebuild contracts and recalculate test contract addresses

--- ethereum/gateway-eth-ts/src/service/testUtils.ts
+++ ethereum/gateway-eth-ts/src/service/testUtils.ts
@@ -17,7 +17,7 @@ export const gatekeeperNetwork = 1n;
 // These addresses are the ones that hardhat deploys to in the local test environment
 // Note, they differ from the default create2 addresses used in production
 export const TEST_GATEWAY_TOKEN_ADDRESS = {
-  gatewayToken: "0xAa8AC982523DefB670D7DEA8f2da044Ac0DE644d",
+  gatewayToken: "0xAE7d78c9e63B7d47423879B7ca1Bd92E19c7B410",
   forwarder: "0xc2Be8620Cdf7b0742BAc8d7182743aE3C2c3D343",
-  flagsStorage: "0x3dabebce03B587CE0BeA136FEd40C1B7534E59d9",
+  flagsStorage: "0x4D059AF4A04304C55e744882fF45f081490f6Cd5",
 };
